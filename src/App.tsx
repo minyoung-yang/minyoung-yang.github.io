f7bbd755f1c4a8edab5c6d551ee0548fd78cd058
gabriel.yang <gabriel.yang@kakaocorp.com>
2025-07-01 10:29:27 +0900

    project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
+  return <>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,8 +26,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </>;
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '../assets/css/style.scss';
+import App from './App';
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <App />
     </StrictMode>,
-  )
+  );
 }
