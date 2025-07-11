af3a3e429758abce85ad20853e3d634a4d428c2f
gabriel.yang <gabriel.yang@kakaocorp.com>
2025-07-11 09:54:54 +0900

    빌드 에러 수정을 위해 파일 이름 변경 (테스트)

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { About } from "../pages/about";
+import { About } from "../pages/About";
 
 export const Route = createFileRoute("/about")({
   component: About,
