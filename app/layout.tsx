366c6b6c9802a261c8a6e70d5be4df716e987d7e
gabriel.yang <gabriel.yang@kakaocorp.com>
2025-08-03 19:23:28 +0900

    애드센스 광고 삽입을 위한 소유권 구문 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7955956906960062"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   );
