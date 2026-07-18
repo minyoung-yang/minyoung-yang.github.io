97cbd0fc58462ee24042f1ded66904746e322518
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-19 01:38:38 +0900

    48번 포스팅

---
+++ src/data/posts/48/content.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+
+export const content = (
+  <>
+    <p>
+      2025년 5월 1일, 천천히 일어나 조식을 먹었다. 내가 지금까지 먹은 일본
+      호텔의 조식은 모두 맛있었다. 물론 여기도 맛있었다. 배를 채우고 디즈니 씨로
+      향했다. 우린 디즈니 씨에 놀이기구를 타러 가는 것이 아니었다. 차라리 돈을
+      많이 내고 줄 서지 않는 패스권을 살 수 있었다면, 놀이기구를 탔겠지만 디즈니
+      씨에서는 패스권 정보를 구하기가 어려웠다. 줄 서는 것을 싫어하는 우리는
+      그냥 디즈니를 느끼러 갔다. 숙소에서 거리가 멀지 않았지만, 전철을 두 번
+      갈아타야 했다. 세번째 전철은 디즈니 리조트 라인이었는데, 이 전철을
+      기다리면서부터 디즈니 씨의 관광이 시작되었다. 열차의 창문과 손잡이가 모두
+      미키 모양이었다. 디즈니 풍의 음악이 나오고, 디즈니 씨의 관광이 시작되었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/h8G9DBL.jpeg",
+        "https://i.imgur.com/aQhprdb.jpeg",
+        "https://i.imgur.com/RNYHFqq.jpeg",
+      ]}
+      alt="디즈니 리조트 라이 열차 내"
+      caption="미키 마우스 모양의 창문과 손잡이가 디즈니 씨로 안내한다."
+    />
+    <p>
+      입구부터 사람이 붐빈다. 일부러 오픈런도 안하고 천천히 왔는데, 아직도
+      사람이 많다. 느긋하게 줄서서 입장했다. 들어가서도 사람이 많았다. 일본의
+      황금 연휴, 골든위크였다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/JmtsRe1.jpeg",
+        "https://i.imgur.com/AJ710E1.jpeg",
+        "https://i.imgur.com/GEkDl78.jpeg",
+      ]}
+      alt="디즈니 씨"
+      caption="사람 너무 많다."
+    />
+    <p>
+      기억 나는 것이 별로 없을 정도로, 우리는 가벼운 구경만 했다. 디즈니 굿즈
+      몇개 사고, 디즈니 느낌이 많이 나는 공간 구경만 계속 했다. 유니버설
+      스튜디오처럼 출구에 지구 모형이 있길래, 그걸 마지막으로 찍고 빠르게
+      나왔다. 주변에 이렇게 얘기하면, 다들 놀랜다. 어떻게 놀이 공원에 가서 놀이
+      기구를 하나도 안 타냐고. 근데 뭐, 나름 만족하게 잘 놀다 왔다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/JyEroSG.jpeg",
+        "https://i.imgur.com/EgImbec.jpeg",
+        "https://i.imgur.com/ydIfiK1.jpeg",
+      ]}
+      alt="디즈니 씨"
+      caption="디즈니 씨의 분위기"
+    />
+    <p>
+      5월의 첫번째 날이었지만, 하루종일 야외에서 걷다보니 땀이 나고 몸이 지쳤다.
+      우린 잠시 숙소로 돌아갔다. 약 한 시간의 휴식 후, 쇼핑하러 신주쿠로
+      이동했다. 오가와마치 역을 통해 신주쿠 선을 타고 가니 금방이었다. 숙소
+      위치를 잘 잡았다. 우린 도쿄에 오기 전부터 일본에서 사야할 것들을 리스트로
+      적어뒀다. 가장 먼저 시로에 가서 향수를 구경했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/G0xNU8V.jpeg",
+        "https://i.imgur.com/C1WwTeq.jpeg",
+      ]}
+      alt="시로 향수"
+      caption="우리는 각자 하나씩 구입했다."
+    />
+    <p>
+      여행 오기 전에 알아둔 초밥 식당으로 이동했다. 근데 뭐랄까, 참 익숙한
+      곳이었다. 언젠가 와본 것만 같은. 다시 한번 구글 맵을 보니, 내가 남긴
+      리뷰가 있었다. 6~7년 전에 도쿄에 방문했을 때, 일본에 사는 선배가 방문
+      기념으로 먹여준 초밥 식당이었다. 그 당시에 가난한 대학원생이었기에, 맛에
+      비해 가격이 비싸다는 리뷰를 남겼었다. 이번엔 어떨까. 각자 좋아하는 초밥
+      세트와 술을 시켜보니 생각보다 가성비가 좋았다. 물론 맛도 좋았다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/aGwZByi.jpeg"]}
+      alt="시로 향수"
+      height={500}
+      caption="초밥 사진을 못찍었다."
+    />
+    <p>
+      일본 가면 사야하는 꼼데가르송에 들러 우리의 옷과 양가족의 옷을 구입했다.
+      그리고 조금 걸어, 신주쿠 역 서쪽에 있는 리쿼샵에 갔다. 상당히 저렴한
+      가격에 위스키를 구입할 수 있었다. 사고 싶던 위스키와, 한국에 비교적 저렴한
+      위스키를 골라 면세가로 구입했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/HXmcm6b.jpeg",
+        "https://i.imgur.com/kAiRMvN.jpeg",
+        "https://i.imgur.com/bq6QDGA.jpeg",
+      ]}
+      alt="리쿼샵과 꼼데가르송"
+      caption="돈 좀 썼다."
+    />
+    <p>
+      쇼핑을 즐기고, 아키하바라로 돌아왔다. 각종 크레인 뽑기와 가챠를 즐겼다. 꽤
+      뽑기 쉬워보이는 젤다의 전설 링크 피규어가 있길래, 약 2,000엔을 투자하여
+      뽑았다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/ovatqMf.jpeg",
+        "https://i.imgur.com/peGHLyc.jpeg",
+        "https://i.imgur.com/Y50q2YM.jpeg",
+      ]}
+      alt="아키하바라"
+      caption="한국에선 거들떠도 보지 않았겠지만, 아키하바라에서는..."
+    />
+    <p>
+      우리가 머무르는 숙소에서는 야식을 제공한다. 라멘이라고 하는데, 먹어보면
+      꽤나 맛있는 국수 느낌이다. 차가운 녹차와 함께 냉탕 온탕을 넘어다니며,
+      하루를 마무리한다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/qBlJWlD.jpeg"]}
+      alt="라멘"
+      caption="멸치국수 같기도 하다."
+      height={500}
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/ZPPp2Q2Pztt8yJQa6"
+      title="시로 향수"
+      description="SHIRO 伊勢丹新宿店"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/diVK6fu9K2NnKb5H8"
+      title="꼼데가르송"
+      description="COMME des GARCONS"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/54H9KnKb1QaHM5pbA"
+      title="저녁 / 스시"
+      description="스시잔마이 신주쿠히가시구치점"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/dRJzwxTHmpHT5EEW9"
+      title="리쿼샵"
+      description="야마야 신주쿠점"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/48/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "48",
+  title: "2. 디즈니 씨 느낌만 보고, 쇼핑하러",
+  slug: "disney-sea-shopping",
+  summary: "도쿄 여행 2일차, 디즈니 씨와 신주쿠 쇼핑",
+  subCategoryId: "3",
+  author: "양가브리엘",
+  publishDate: "2026-07-19 01:36:00",
+  readTime: 5,
+  imageUrl: "https://i.imgur.com/GEkDl78.jpeg",
+  tags: ["디즈니 씨", "쇼핑"],
+};
