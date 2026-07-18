8deca9868d0420d9ebf572a4c105ee910acbe5a6
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-18 22:14:55 +0900

    47번 포스팅

---
+++ src/data/posts/47/content.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+
+export const content = (
+  <>
+    <p>
+      2025년 4월 30일, 오전에 일찍 일어나 인천공항으로 출발했다. 5년만에,
+      오랜만에 가는 도쿄지만 그다지 설레지는 않는다. 이미 세 번이나 다녀오기도
+      했고, 일본의 다른 지역을 더 보고 싶었기 때문이다. 그나마 조금 설레는 것든,
+      내가 5년 동안 변했기 때문에 여행이 다를 것이라는 점이었다. 여행에 대한
+      가치관이 잡혔으며, 쇼핑에 대한 여유가 생겼다는 점이었다. 그렇게 설레는
+      맘으로 와이프와 함께 출발했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/cUw0PPd.jpeg",
+        "https://i.imgur.com/nI312iv.jpeg",
+      ]}
+      alt="비행기에서 본 일본"
+      caption="어딘지 모르겠다. 만년설인가?"
+    />
+    <p>
+      숙소는 오타쿠의 성지 아키하바라로 잡았다. 별다른 이유는 없었고, 교통이
+      편하면서 조식도 나오고, 대욕장도 있는 곳을 필터링해서 찾다보니
+      아키하바라였다. 역으로 치면, 아키하바라 역, 간다 역, 오가와마치 역,
+      이와모토초 역, 총 네 개의 역 사이에 있었기 때문에 아주 편리했다. 나리타
+      공항에 도착하자마자, 스카이라이너를 타고 게이세이우에노 역으로 이동했다.
+      조금 걸어 우에노 역으로 이동 후, 긴자 라인을 타고 간다 역에서 내려
+      숙소까지 걸어갔다. 간다 역에서 꽤 북쪽까지 지하도가 있어 이동에 편리했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/rP58Ra9.jpeg",
+        "https://i.imgur.com/LWXRXM4.jpeg",
+        "https://i.imgur.com/YUxmxmq.jpeg",
+      ]}
+      alt="스카이라이너 내부"
+      caption="스카이라이너는 앞뒤 좌석이 넓어서 편했다."
+    />
+    <p>
+      숙소에 체크인하니 오후 8시였다. 빠르게 짐 정리 후, 저녁 식사하러 나왔다.
+      도로에는 관광객들로 가득 차있었다. 웬만한 음식점에는 모두 웨이팅이 길었다.
+      이때까지는 깨닫지 못했다. 일본 환율이 매우 낮아져서 관광객이 많다고
+      생각했다. 물론 그 이유도 있었지만, 우린 여행 일정을 잘못 잡은 것이었다.
+      4월 말부터 5월 초까지는 일본에서 골든위크로 불리는 연휴이다. 물론
+      한국에서도 연휴로 부를만큼 공휴일이 많지만, 일본에서는 공휴일이 다섯 개나
+      있는 말 그대로 황금 연휴이다. 가성비와 비수기를 좋아하는 나로서는 일정을
+      매우 잘못 선택한 것이다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/HAsX6qc.jpeg",
+        "https://i.imgur.com/CtuA4k4.jpeg",
+      ]}
+      alt="아키하바라 중심가"
+      caption="큰 도로에는 사람이 별로 없어보인다. 하지만 모든 음식점에 줄 서있다."
+    />
+    <p>
+      사람이 너무 많아, 웨이팅 없이 먹을 수 있는 식당이 없었다. 애매하게 줄 서서
+      먹느니, 관광객들에게 유명한 맛집에 줄 서서 먹기로 했다. 우리가 간 곳은
+      스테이크 덮밥 식당이었다. 내 기억에 60분 정도 줄 섰던 것으로 기억한다.
+      계단이 좁았기에 줄 서 있는 것도 상당히 피곤했다. 하지만 어쩌겠나.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/bcmzXej.jpeg",
+        "https://i.imgur.com/lG9MRMh.jpeg",
+      ]}
+      alt="스테이크 덮밥 식당에 줄서서"
+      caption="식당에서는 회전율을 높이기 위해 주문을 미리 받는다."
+    />
+    <p>
+      뭘 시켰는지 기억이 잘 안난다. 아마 나는 엑스라지 사이즈, 와이프는 미디움
+      사이즈를 먹었던 거 같다. 당연히 맛있었다. 한우에 버금가는 와규이며, 적당한
+      굽기로 맛있는 소스와 밥을 곁들어 먹기 때문이다. 고기가 매우 부드럽고
+      감칠맛 나서, 햄인가 싶기도 했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/MEwziJZ.jpeg",
+        "https://i.imgur.com/sGzgGMa.jpeg",
+      ]}
+      alt="스테이크 덮밥 식당"
+      caption="어떻게 먹을지 친절하게 한국어로 알려준다."
+    />
+    <p>
+      길게 대기하고, 빠르게 먹고, 숙소로 돌아가기 전 아키하바라 중심지를
+      구경했다. 큰 상가는 꽤 늦은 시간인 10시까지 운영하고 있었다. 빅 카메라와
+      요도바시 카메라를 구경했다. 위스키 가격을 가볍게 살펴보고, 이번에 일본에서
+      구입하고 싶었던 코털 정리기를 살펴봤다. 자세히 살펴보기 어렵게, 늦은
+      시간이었으므로 우리는 물건 위치만 확인하고 나왔다. 숙소로 돌아가는 길,
+      몇몇 가챠샵에서 돈좀 써주고 숙소로 돌아왔다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/lHZNj2c.jpeg",
+        "https://i.imgur.com/VtNW1hH.jpeg",
+        "https://i.imgur.com/cXCRkIP.jpeg",
+      ]}
+      alt="빅카메라와 요도바시카메라"
+      caption="일본의 위스키는 정말 저렴하다."
+    />
+    <p>
+      일본에서의 하루 마무리는 무조건 편의점 간식과 맥주다. 편의점에서 먹고 싶은
+      것을 다 고르고, 숙소로 돌아와 대욕장에서 피로를 풀었다. 그리고 우린 맥주와
+      함께 하루를 마무리했다.
+    </p>
+    <ImageInEntry
+      src="https://i.imgur.com/P05N4DE.jpeg"
+      alt="편의점 간식과 맥주"
+      caption="내가 마신게 뭔지 기억나지 않는다."
+      height={400}
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/xKv6RzpjoeRzr6YV9"
+      title="아키하바라 숙소"
+      description="도미 인 프리미엄 칸다"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/XEZzJXaV7Ui8Ytho9"
+      title="저녁 식사 / 스테이크 덮밥 식당"
+      description="ローストビーフ大野 秋葉原店"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/47/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "47",
+  title: "1. 5년만에 도쿄로, 아키하바라",
+  slug: "tokyo-akihabara",
+  summary: "도쿄 여행 1일차, 인천 공항을 거쳐 아키하바라로",
+  subCategoryId: "3",
+  author: "양가브리엘",
+  publishDate: "2026-07-18 22:14:00",
+  readTime: 5,
+  imageUrl: "https://i.imgur.com/HAsX6qc.jpeg",
+  tags: ["인천국제공항", "도쿄", "아키하바라", "나리타공항"],
+};
