f2601d3ab961ab246b326a3514626bc72c02f8fb
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-20 00:30:50 +0900

    50번 포스팅

---
+++ src/data/posts/50/content.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+
+export const content = (
+  <>
+    <p>
+      2025년 5월 3일, 조식을 먹고 이케부쿠로로 향했다. 숙소 근방의 아와지초 역을
+      통하니 금방이었다. 이케부쿠로는 오타쿠들의 성지로 유명한 장소이다. 구
+      성지가 아키하바라였다면, 신 성지가 이케부쿠로라고 한다. 그런 캐릭터
+      굿즈들을 구경하고 사고 싶었기에 이케부쿠로 선샤인 시티로 갔다. 선샤인
+      시티에는, 얼마 전 비극적인 사건으로 인해 잠시 휴업 중인, 포켓몬 센터가
+      있다. 조카가 블랙 리자몽이 갖고 싶대서, 들어가자마자 블랙 리자몽만
+      찾아다녔다. 근데 없더라. 갖고 싶은거 없는지 보이스톡 걸어서 이것저것
+      물어봐도, 시원찮은 답변만 나오길래 그냥 이것저것 샀다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/4Nyzy5Y.jpeg",
+        "https://i.imgur.com/ohzf5H8.jpeg",
+        "https://i.imgur.com/wTYL8ux.jpeg",
+      ]}
+      alt="포켓몬 인형 및 피규어"
+      caption="나도 포켓몬 세대다. 블랙 리자몽보다는 오리지널이 이쁜듯"
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/8GEaCUj.jpeg",
+        "https://i.imgur.com/XjYebAt.jpeg",
+        "https://i.imgur.com/t25TPBc.jpeg",
+      ]}
+      alt="포켓몬센터"
+      caption="정말 뻑이 가게 이쁘게 꾸며놓은 포켓몬센터"
+    />
+    <p>
+      선샤인 시티에는 포켓몬센터 뿐 아니라, 여러 캐릭터, 게임의 굿즈샵이 많았다.
+      가챠 기계도 많았기에, 우리는 시간 가는줄 모르고 돌아다녔다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/ertyo78.jpeg",
+        "https://i.imgur.com/GXWP1c9.jpeg",
+        "https://i.imgur.com/rwmcDtY.jpeg",
+      ]}
+      height={300}
+      alt="다양한 굿즈샵"
+      caption="뭔지 몰라도, 뭔지 알아도, 재밌게 구경할 수 있다."
+    />
+    <p>
+      피규어들을 뒤로하고.. 스냅 촬영을 예약해뒀기에, 시간을 맞춰야했다. 줄서지
+      않는 식당을 찾아 빠르게 라멘을 한 뚝배기 하고, 약속 장소인 닛포리로
+      향했다. 결혼을 앞두었기에, 여행 겸 웨딩 스냅을 찍으려는 것이었기에, 닛포리
+      역에 있는 꽃집에 들러 작은 꽃다발을 하나 샀다. 사진 작가 선생님을 만나,
+      가벼운 영어로 의사소통 하며 촬영을 시작했다. 어디서 찍을지 철저히 조사하여
+      고른 닛포리 역이었는데, 아주 만족스러웠다. 다만, 골든위크로 인해 사람이
+      너무 많은 것이 함정이었다. 한 시간의 촬영을 마쳤다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/OByZcJf.jpeg",
+        "https://i.imgur.com/1XSKs3i.jpeg",
+      ]}
+      alt="닛포리역 근방"
+      caption="일본하면 떠오르는 느낌. 닛포리 역 근방에서 느낄 수 있다."
+    />
+    <p>
+      닛포리 역 근방은 요즘 야네센이라고 불리는 인기 스팟이다. 긴 시간 있지
+      않았지만, 다시 도쿄에 방문한다면 또 다시 가고 싶은 스팟이다. 이대로 떠나긴
+      아쉬워서, 열려있는 꼬치 식당에 들어가 낮맥주를 한잔 때리고 나왔다.
+      한글/영어 메뉴도 준비되어 있지 않은 찐 로컬 식당이라서, 추천 꼬치 다섯개를
+      달라고 한 뒤 "나마비루"를 시켜먹었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/kkumoVz.jpeg",
+        "https://i.imgur.com/OitfgET.jpeg",
+        "https://i.imgur.com/aMloZNK.jpeg",
+      ]}
+      alt="꼬치 식당"
+      caption="그 어떤 음식도 나무 꼬챙이에 끼워 구운뒤 데리야끼 소스를 부으면 맛있다."
+    />
+    <p>
+      숙소로 돌아가는 길, 지인이 추천해준 장소를 들르기 좋은 타이밍이었다. 그
+      장소는 오차노미즈 역이었는데, 스즈메의 문단속 한 장면이라며 더 유명해졌다.
+      각기 다른 종류의 열차와 하천이 어우러져 일본 특유의 분위기를 풍긴다. 해가
+      살랑 살랑 질 무렵이었기에 더 느낌 좋은 곳이었다. 볼 건 이 뷰 하나뿐이지만
+      20분 정도 멍하니 쳐다보다 숙소로 돌아와 하루를 마쳤다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/PwFBucO.jpeg"]}
+      alt="오차노미즈 역"
+      caption="그냥 멍하니 열차가 지나다니는 것을 쳐다본다."
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/yB7HVw2cbLj1XcKM6"
+      title="꼬치 식당"
+      description="Yakitori Imaichi"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/50/meta.tsx
@@ -0,0 +1,15 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "50",
+  title: "4. 오타쿠들의 성지 이케부쿠로와 닛포리에서의 스냅 촬영",
+  slug: "tokyo-ikebukuro-nippori",
+  summary:
+    "도쿄 여행 4일차, 오타쿠들의 성지 이케부쿠로와 닛포리에서의 스냅 촬영",
+  subCategoryId: "3",
+  author: "양가브리엘",
+  publishDate: "2026-07-20 00:28:00",
+  readTime: 4,
+  imageUrl: "https://i.imgur.com/PwFBucO.jpeg",
+  tags: ["도쿄", "이케부쿠로", "닛포리", "오차노미즈"],
+};
