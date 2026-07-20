8221d71c2884e757afa8abd6a5b9bbb592cf2541
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-21 00:23:19 +0900

    51번 포스팅

---
+++ src/data/posts/51/content.tsx
@@ -0,0 +1,231 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+
+export const content = (
+  <>
+    <p>
+      2025년 5월 4일, 가장 기대하는 날이다. 그동안 도쿄에 방문하며 한번도 가보지
+      않았던 곳을 알게 되었고, 그 계획을 실행하는 날이다. 가마쿠라에는 자연을
+      품은 사찰이 있는 시골 동네다. 요즘 더 유명해진 것은, 남쪽으로 쭉 가면
+      슬램덩크에 나오는 전차와 고등학교가 있기에 슬램덩크 성지순례길로도
+      유명해졌다. 숙소에서 간다 역을 지나 시나가와 역으로 이동, 전철을 갈아타서
+      기타카마쿠라 역으로 이동했다. 내리자마자 깨달았다. 여행지 잘 골랐다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/ZRrJrn8.jpeg"]}
+      alt="전철에서 창밖을 본 풍경"
+      caption="점점 시골로 변해가는 창 밖"
+    />
+    <p>
+      일본 애니메이션에 나올 법한 시골 기차길이 우릴 반겼다. 빨리 역 바깥으로
+      나가 구경하고 싶었다. 우리가 갈 코스는 기타카마쿠라 역부터 카마쿠라 역까지
+      도보로 걷는 것이다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/8olS8aJ.jpeg",
+        "https://i.imgur.com/e8qZEao.jpeg",
+      ]}
+      alt="기타카마쿠라 역"
+      caption="풀냄새 나는 기타카마쿠라 역"
+    />
+    <p>
+      먼저 엔가쿠지에 방문했다. 사실 뭐하는 곳인지 모르고, 궁금하지도 않다. 그냥
+      숲 속에 있는 오래된 일본 풍 건물과 분위기를 느끼고 싶었다. 자연 속에
+      있지만, 관리는 되고 있는, 기분이 좋아지는 곳이었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/DsPm1Ty.jpeg",
+        "https://i.imgur.com/MDZ1UWL.jpeg",
+        "https://i.imgur.com/dpfXLFe.jpeg",
+      ]}
+      alt="엔가쿠지"
+      caption="엔가쿠지는 첫번째 스팟으로써 성공적이었다."
+    />
+    <p>
+      조금 걸어 죠치지로 이동했다. 여기도 엔가쿠지와 비슷한 곳이었다. 입장료를
+      냈던거 같기도 하고. 죠치지는 엔가쿠지에 비해 더 넓었다. 사찰보다 자연
+      중심적이었다. 둘 중 하나를 선택하자면, 죠치지가 더 좋았다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/nuJEY6k.jpeg",
+        "https://i.imgur.com/UNQNUVc.jpeg",
+        "https://i.imgur.com/CGjkRtX.jpeg",
+      ]}
+      alt="죠치지"
+      caption="죠치지도 성공적이었다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/HNTeb5Z.jpeg",
+        "https://i.imgur.com/rbZikSl.jpeg",
+        "https://i.imgur.com/KMTr9H1.jpeg",
+      ]}
+      alt="죠치지"
+      caption="가까운 곳이었다면, 한달에 한 번은 산책하러 갔을텐데"
+    />
+    <p>
+      죠치지를 나와 가마쿠라 역으로 이동했다. 차가 지나갈 수 없는 산길로
+      이동했다. 이동 중 커피를 마시기 위해 길거리 카페에 잠시 멈췄다. 커피 한
+      잔과 콩맛 소프트 아이스크림을 하나 샀다. 와이프는 이 때 먹은 아이스크림이
+      정말 맛있었다고 한다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/kGe3Bm9.jpeg",
+        "https://i.imgur.com/KUrda8M.jpeg",
+      ]}
+      alt="가마쿠라 역으로 가는 길"
+      caption="좁은 도로를 따라 걷다보면 작은 음식점과 카페들이 있다."
+    />
+    <p>
+      차는 다닐 수 없는 산길에 들어갔다. 기억에 우리 외에는 아무도 없었다.
+      그렇다고 무서운 분위기는 아니었지만, 신기한 분위기였다. 이럴 때 종종
+      얘기하는 것이 센과 치히로처럼, 새로운 동네가 아닌 새로운 세상으로 가는
+      듯한 느낌이 들었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/hgBxw66.jpeg",
+        "https://i.imgur.com/BBw8yCy.jpeg",
+        "https://i.imgur.com/AQ3lWec.jpeg",
+      ]}
+      alt="가메가야츠 고개"
+      caption="아무 것도 모르고 걷다보니, 가메가야츠 고개라는 한글 설명이 있었다."
+    />
+    <p>
+      산길이 끝나면 작은 주택 단지가 보인다. 짱구 집보다는 조금 더 시골 같고.
+      그럼에도 비싸보이는 시골집이다. 주택단지 바로 앞에는 전차길이 있었는데,
+      일본은 참 기차/전차에 친화적인 듯 하다. 우리나라였으면 전차의 소음으로
+      인해 주택단지나 전차 둘 중 하나는 사라졌을 것이다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/qpaMC8r.jpeg",
+        "https://i.imgur.com/SwdPnzt.jpeg",
+      ]}
+      alt="가마쿠라 주택단지"
+      caption="부자들이 사는 동네 같다."
+    />
+    <p>
+      걷다보니 가마쿠라 역 앞이었고, 배를 채우기 위해 근처 식당에 들어갔다.
+      우리는 일본 음식집 중, 타코야끼와 볶음우동, 오코노미야끼를 파는 식당을
+      좋아하는데, 마침 그런 식당이 있고 평점도 좋았다. 아기자기한 분위기에 맛
+      좋은 식당이었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/2OLk6ad.jpeg",
+        "https://i.imgur.com/WvRRocY.jpeg",
+        "https://i.imgur.com/yelrQjy.jpeg",
+      ]}
+      alt="음식"
+      caption="우리가 좋아하는 세 가지 메뉴를 모두 시켰다."
+    />
+    <p>
+      배도 채웠기에, 전차를 타고 에노시마 섬으로 향했다. 하지만, 이 날은 골든
+      위크 중 골든 데이였다. 전차 역에는 사람이 매우 많았고, 전차를 세 대 정도는
+      보내야 탈 수 있어 보였다. 시간 뿐 아니라, 전차는 사람으로 가득 찼고,
+      위험해보였다. 우린 그렇게 에노시마를 포기했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/Rla0B6H.png"]}
+      alt="전차역"
+      caption="혹시 몰라, 돌아왔다. 사고가 터질 것처럼 보였다."
+      height={450}
+    />
+    <p>
+      목적지를 고민하다가, 비가 와서 포기했던 하라주쿠로 향했다. 메이지 진구를
+      산책했다. 메이지 진구를 떠날 때 사람들은 목례를 하며 나오는데, 한국에
+      역사적 아픔을 준 인물을 봉안한 곳이기에 목례나 합장을 하지 않도록 주의해야
+      한다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/OT3cIGl.jpeg",
+        "https://i.imgur.com/8cMFmJD.jpeg",
+      ]}
+      alt="메이지 진구"
+      caption="넓직한 공원을 산책하는 느낌"
+    />
+    <p>
+      하라주쿠에서 쇼핑 좀 해볼랬지만, 사람이 너무 많아 포기했다. 러닝화나 하나
+      구입할까하여 호카에 30분 정도 줄서서 입장했지만, 가격이 저렴하지도 않았다.
+      이제 어디를 가야할지 고민 중에 우선 식사부터 하기로 했다. 일본 가정식을
+      판매하는 식당이었고, 돈까스와 카레를 주문했다. 깔끔하고 맛있었다. 근데
+      돈까스는 일본에서 먹는 돈까스보다 한국의 일식 돈까스가 더 맛있는거 같다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/UWdyLFa.jpeg",
+        "https://i.imgur.com/x53bGNW.jpeg",
+        "https://i.imgur.com/JhjRrPH.jpeg",
+      ]}
+      alt="일본 가정식 식당"
+      caption="깔끔한 맛"
+    />
+    <p>
+      식사하며 우리의 다음 목적지를 정했다. 걸어서 시부야까지. 시부야는 와이프가
+      즐겨하던 게임인 &lt;페르소나 5&gt;의 주배경이다. 어쩌면 이 포인트가 시부야
+      방문에 큰 영향을 미쳤을거 같다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/wcWkBZj.jpeg",
+        "https://i.imgur.com/YpEbsoU.jpeg",
+        "https://i.imgur.com/rv0Ocqi.jpeg",
+      ]}
+      alt="시부야"
+      caption="늘 그렇듯 우리는 포켓몬센터와 닌텐도스토어를 구경했다."
+    />
+    <p>
+      그렇게 사람이 많이 다닌다는 시부야 스크램블에, 그렇게 사람이 많다는
+      골든위크에 갔다. 게임에 나오는 주요 스팟과 인파 속 빌딩들을 구경한 후
+      숙소로 돌아갔다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/bskntZU.jpeg"]}
+      alt="시부야 스크램블"
+      caption="안 그래도 사람이 많은 곳인데, 더 많다."
+    />
+    <p>
+      여행 스킬이 올라서일까. 이번 도쿄 여행에서 꽤나 좋은 곳을 찾아낸거 같다.
+      언젠가 다시 도쿄에 방문할 일이 생긴다면, 가마쿠라를 비롯하여 그와 비슷한
+      곳들을 발굴하고 방문해보려 한다.
+    </p>
+    <OgLink
+      href="https://maps.app.goo.gl/8RDn6R5TTP5dUgrE7"
+      title="엔가쿠지"
+      description="円覚寺"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/zkxs2DaimE2Q6ZSD9"
+      title="죠치지"
+      description="浄智寺"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/5MzbyFofxCSERvQi6"
+      title="콩 아이스크림 카페"
+      description="Kanroya"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/2SYKEMAUcyPC8bU79"
+      title="점심 / 타코야끼 식당"
+      description="타이 코오보오"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/fCV4uNPTr1UnPfot8"
+      title="저녁 / 일본 가정식"
+      description="The Teisyoku Shop"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/51/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "51",
+  title: "5. 가마쿠라와 하라주쿠를 거쳐 시부야까지",
+  slug: "kamakura-harajuku-shibuya",
+  summary: "도쿄 여행 5일차, 가마쿠라의 사찰과 하라주쿠/시부야의 건물 숲",
+  subCategoryId: "3",
+  author: "양가브리엘",
+  publishDate: "2026-07-21 00:22:00",
+  readTime: 5,
+  imageUrl: "https://i.imgur.com/SwdPnzt.jpeg",
+  tags: ["도쿄", "가마쿠라", "하라주쿠", "시부야"],
+};
