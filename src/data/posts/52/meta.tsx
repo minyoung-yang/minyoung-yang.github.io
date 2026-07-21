6bece3e933337876d94183b393a81f15592b9da2
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-21 23:46:06 +0900

    52번 포스팅

---
+++ src/data/posts/52/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "52",
+  title: "6. 아사쿠사를 마지막으로",
+  slug: "tokyo-asakusa",
+  summary: "도쿄 여행 6일차, 아사쿠사를 마지막으로 도쿄 여행을 마치며",
+  subCategoryId: "3",
+  author: "양가브리엘",
+  publishDate: "2026-07-21 23:45:00",
+  readTime: 3,
+  imageUrl: "https://i.imgur.com/pNgskqU.jpeg",
+  tags: ["도쿄", "아사쿠사"],
+};
