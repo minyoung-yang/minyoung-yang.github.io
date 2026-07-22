fc4d3ed84a215490fe1af6a7554cfc5dbf461975
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-23 01:31:49 +0900

    53번 포스팅

---
+++ src/data/posts/53/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "53",
+  title: "1. 로키 산맥 캠핑카 투어를 캘거리에서 시작하다.",
+  slug: "rocky-camping-car-tour-start",
+  summary: "로키 산맥으로 가기 위해 캘거리로 이동",
+  subCategoryId: "9",
+  author: "양가브리엘",
+  publishDate: "2026-07-23 01:30:00",
+  readTime: 5,
+  imageUrl: "https://i.imgur.com/8RJo4yt.jpeg",
+  tags: ["캐나다", "캘거리", "로키산맥", "캠핑카", "밴프"],
+};
