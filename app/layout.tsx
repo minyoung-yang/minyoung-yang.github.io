6a26a172caae7f619923eeb4beed9d4372af5550
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-24 00:44:08 +0900

    네이버 서치어드바이저 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7955956906960062"
           crossOrigin="anonymous"
         ></script>
+        <meta
+          name="naver-site-verification"
+          content="bd3ca25b65db38024528a993afba434724bd16bb"
+        />
       </head>
       <body>{children}</body>
     </html>
