342da943f4207606c449eae8ecbc15dd00c582e3
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-24 00:35:23 +0900

    54번 포스팅

---
+++ src/data/posts/54/content.tsx
@@ -0,0 +1,188 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+import { YoutubeVideo } from "@/components/entry-detail/youtube-video";
+
+export const content = (
+  <>
+    <p>
+      2025년 9월 30일, 쉬엄 쉬엄 움직이는 날이었다. 첫 날은 비행기로 이동하느라,
+      둘째 날은 캠핑카 대여부터 적응하느라. 그리고 피로가 쌓인 셋째 날이었다.
+      느즈막히 일어났던 거 같다. 우리 가족은 아침 식사를 차려먹고, 10시 30분 쯤
+      출발했다. 오늘의 목적지인 밴프 타운은 캠핑장부터 매우 가까웠다. 마음 먹고
+      걸어갈 수 있을 정도다. 지도를 검색해보니 3km 정도다. 우린 버스를 타고
+      이동했다. 한 명당 2달러, 캐나다 달러니까 2,000원이다. 현금을 준비했는데,
+      정류장에 적혀있는 글을 읽어보니 앱으로만 가능하다고 한다. 급하게 앱을
+      설치하고, 세 명 치 티켓을 구입하여 탑승했다. 버스 탑승하기 직전부터 약간의
+      비가 내렸다. 밴프 타운에서 내려, 굿즈샵들을 구경하며 걸었다. 어떤 브랜드
+      샵에서는 밴프 타운 티셔츠도 판매 중이었다. 웬만한 관광지에서는 싸구려
+      티셔츠를 판매하는데, 여기서는 믿을 만한 브랜드로 기념 티셔츠를 판매하기에
+      합리적인 품질을 기대하며 구입했다. 조카의 옷을 Herschel에서 구입하고,
+      Mountain Warehouse에서 내 옷을 구입했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/dEkfS3g.jpeg",
+        "https://i.imgur.com/WNvYUfM.jpeg",
+      ]}
+      alt="밴프 타운"
+      caption="꾸리꾸리한 날씨"
+    />
+    <p>
+      많이 걷다 보니 지쳤다. 우리 부모님은 점심 식사를 자주 거르시는데, 나에게는
+      있을 수 없는 일이라 식당으로 모셨다. 부모님은 하나를 시켜 나눠드시고, 난
+      햄버거와 맥주를 시켜먹었다. 물론 한국보다 비싼 가격이었지만, 꽤 맛이
+      좋았다. 3일 중 처음으로 팁을 결제한 날이었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/a7lClfx.jpeg",
+        "https://i.imgur.com/AezqCFU.jpeg",
+      ]}
+      alt="음식"
+      caption="꾸리한 날씨와 맥주와 햄버거. 환상의 조합"
+    />
+    <p>
+      식사를 마치고 밴프타운을 가볍게 산책하다가 캠핑장으로 돌아왔다. 잠시 쉴 겸
+      각자의 자리에 누워있었는데, 두 시간 정도 잠에 들었다. 일어나니 아버지는
+      계속 주무시고 계셨고, 어머니께서 주변에 다녀오자고 하셨다. 먼 로키
+      산맥까지 왔는데, 제대로 트레킹을 못한게 많이 아쉬우셨던거 같다. 캠핑장
+      주변에 선택할 수 있는 트레킹 코스가 있어서, 무난한 길로 선택했다. 왕복
+      40분 정도 걸린 것으로 기억한다. 엄청난 뷰가 있는 것은 아니었지만, 일상적인
+      뷰는 아니었다. 산으로 둘러싸여 있고, 영어로 된 안내판과 처음 보는 새들로
+      가득했다. 목적지에는 후두스 (hoodoos)라는, 수백만년에 걸쳐 풍화와 침식
+      작용으로 형성된 버섯 모양의 바위가 있었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/gZ0sii3.jpeg",
+        "https://i.imgur.com/7IsmYzS.jpeg",
+      ]}
+      alt="산"
+      caption="로키 산맥으로 둘러싸인 트레킹 코스"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/LxrS63J.jpeg"]}
+      alt="산"
+      caption="AI로 검색해보니 Canada Jay라는 새. 매형인줄."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/OLNu6iM.jpeg"]}
+      alt="후두스"
+      caption="목적지였던 후두스. 대단하진 않았다."
+    />
+    <p>
+      캠핑카로 다시 돌아오니 슬슬 해가 지고 있었다. 트레킹을 시작할 때에는
+      잠에서 깬지 얼마 되지 않아 몸이 떨렸는데, 좀 걷고 오니 몸에 땀이 날랑 말랑
+      했다. 저녁 먹고 잠에 들 준비를 했지만, 지난 낮잠으로 인해 쉽게 잠이 오지
+      않는 밤이었다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/Dc257Dy.jpeg"]}
+      alt="캠핑장"
+      caption="길이지만 아무도 다니지 않는 길이 정말 좋다."
+    />
+    <p>
+      잠에서 깨어 캠핑카를 끌고 밴프 타운으로 이동했다. 전날 날씨로 인해 즐기지
+      못했던 밴프의 뷰를 다시 한번 즐겼다. 코인 빨래방에서 빨래가 돌아가는 시간
+      동안 주변을 돌아다녔다. 필요한 식자재도 구입하고, 맑은 하늘의 마을도
+      구경했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/Nxy86KH.jpeg"]}
+      alt="식자재마트"
+      caption="단순히 가격 표기 때문일까. 같은 마트지만 왜 더 이뻐보이지."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/FLM2G6d.jpeg"]}
+      alt="밴프 타운"
+      caption="전날과는 다른 색깔의 하늘"
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/6ulTgsq.jpeg",
+        "https://i.imgur.com/UBsWT0R.jpeg",
+      ]}
+      alt="밴프 속 한국"
+      caption="밴프에도 한국의 흔적이 있다."
+    />
+    <p>
+      건조까지 마친 뒤, 다음 장소로 떠날 준비를 했다. 배고픔을 참을 수 없는 나는
+      다시 한 번 식사의 중요성을 설명하고 부모님을 식당으로 모셨다. 동남아
+      음식점에서 간단한 식사를 마치고, 밴프와 헤어질 준비를 했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/srDVOjh.jpeg"]}
+      alt="보우강 다리"
+      caption="로키 산맥을 흐르는 보우강, 그리고 다리"
+    />
+    <p>
+      잘 때는 아늑하지만, 가솔린이 살살 녹는 이 차를 끌고 다음 목적지인 레이크
+      루이스로 향한다. 가는 길에 존스턴 캐니언을 들렀다. 왕복 두 시간 정도
+      걸리는 꽤 힘들 수 있는 트레킹 코스였다. 제일 끝까지 올라가면 upper 폭포가
+      있다. 끝까지 올라가지 않는 사람들은 lower 폭포에서 돌아가기도 한다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/7Ka8qZl.jpeg",
+        "https://i.imgur.com/ZhOvHKU.jpeg",
+      ]}
+      alt="폭포"
+      caption="폭포. 대단하진 않았다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/5olJVQv.jpeg",
+        "https://i.imgur.com/CuUjSfM.jpeg",
+        "https://i.imgur.com/jVB2ZEl.jpeg",
+      ]}
+      alt="다람쥐"
+      caption="귀여운 다람쥐 친구들"
+    />
+    <YoutubeVideo
+      videoId="OnlWHdz7BL8"
+      caption="존스턴 캐년의 폭포"
+      height={400}
+    />
+    <YoutubeVideo
+      videoId="4LnESEEHfSU"
+      caption="하 루 종일 시끄럽게 하는 다람쥐"
+      height={400}
+    />
+    <p>
+      로키 산맥에서 제대로된 트레킹을 처음 했다. 점점 이 대자연이 익숙해지고
+      있다. 우린 캠핑카를 타고 레이크 루이스로 향한다.
+    </p>
+    <OgLink
+      href="https://maps.app.goo.gl/usX42QgdRp2PtzoW8"
+      title="기념 티셔츠 샵"
+      description="Herschel"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/YwkfW6AUWt1zS58L6"
+      title="기념 티셔츠 샵"
+      description="Mountain Warehouse"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/EZXaiVnYW4p2bB969"
+      title="햄버거 + 맥주"
+      description="PARK Distillery Restaurant + Bar"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/z6o6Y6nhxP7EVmFV8"
+      title="베트남 음식점"
+      description="Peak Cafe"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/RNVfNgoa9Z6eT2u36"
+      title="존스턴 캐니언"
+      description="Johnston Canyon"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/54/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "54",
+  title: "2. 로키 산맥의 동쪽 관문 밴프",
+  slug: "rocky-banff",
+  summary: "로키 산맥에서 밴프 타운을 지나가는 길",
+  subCategoryId: "9",
+  author: "양가브리엘",
+  publishDate: "2026-07-24 12:35:00",
+  readTime: 5,
+  imageUrl: "https://i.imgur.com/srDVOjh.jpeg",
+  tags: ["캐나다", "로키산맥", "캠핑카", "밴프"],
+};
