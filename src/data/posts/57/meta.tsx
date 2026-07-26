fc63f8b841c3cccced55eb9d6c4d8b18893cf50c
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-26 22:05:26 +0900

    57번 포스팅

---
+++ src/data/posts/57/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "57",
+  title: "10. 3년이 지난 시점에서 이탈리아를 기억하면",
+  slug: "italy-after-3-years",
+  summary: "3년이 지난 시점에서 이탈리아를 기억하다.",
+  subCategoryId: "5",
+  author: "양가브리엘",
+  publishDate: "2025-08-07 23:46:00",
+  readTime: 4,
+  imageUrl: "https://i.imgur.com/s35vear.png",
+  tags: ["이탈리아", "밀라노", "로마", "알프스", "돌로미티"],
+};
