3b22a22471ad21cc2d53f7a1fb5a9aedb9c0da1d
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-28 23:46:04 +0900

    56번 글 포스팅

---
+++ src/data/posts/56/content.tsx
@@ -0,0 +1,215 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+import { YoutubeVideo } from "@/components/entry-detail/youtube-video";
+
+export const content = (
+  <>
+    <p>
+      2025년 10월 4일, 레이크 루이스 캠핑장에서 재스퍼를 향해 출발했다. 구글맵
+      타임라인을 보니, 오전 9시쯤 출발해서 오후 3시 30분 쯤 도착했다. 거리는 약
+      230km로 그렇게 오래 걸릴 건 아니었지만, 뷰포인트가 있을 때마다 내려서
+      쉬고, 취사가 가능한 캠핑카다 보니, 중간에 식사를 하기도 했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/QwAgUeZ.png"]}
+      alt="헥토르 호수"
+      caption="봤던 호수 같지만, 다른 호수다. 헥토르 호수."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ZkRHfRY.jpeg"]}
+      alt="하우스 강"
+      caption="하우스 강을 유유자적 걷는 아버지"
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/vu8XGqd.png",
+        "https://i.imgur.com/8VUxug5.png",
+      ]}
+      alt="폭포"
+      caption="아이스필즈 파크웨이에 있는 브라이들 베일 폭포. 면사포 폭포다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/r4d50rT.png",
+        "https://i.imgur.com/f4DsbTf.jpeg",
+      ]}
+      alt="애써베스카 빙하"
+      caption="애써베스카 빙하는 돈을 내고 투어를 할 수 있다. 저 멀리 투어객이 보인다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ELd4CHR.png"]}
+      alt="눈오는 풍경"
+      caption="일부 구간에서는 폭설이 내리기도 했다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/vQwPwey.jpeg"]}
+      alt="선왑타 폭포"
+      caption="선왑타 폭포에서는 눈이 전혀 안 내린다."
+    />
+    <YoutubeVideo videoId="EIWJ69GC3_8" caption="시원하게 콸콸콸 선왑타 폭포" />
+    <ImageInEntry
+      src={["https://i.imgur.com/8gW2ic2.jpeg"]}
+      alt="애써베스카 폭포"
+      caption="무진장 시원하다. 애써베스카 폭포"
+    />
+    <p>
+      6시간의 장시간 이동을 마치고, 재스퍼 타운에 입성했다. 빨래와 저녁 식사가
+      급했던지라, 사진도 못 찍었다. 마을이 조금은 어수선했다. 2024년 산불로 인해
+      건물이 몇 개 탄 흔적이 있었다. 늦게 도착한 탓에 어두어둑해져서, KFC에서
+      식사를 급하게 마친 후 캠핑장에 체크인하러 이동했다. 중간에 열차 신호등에
+      걸려 정차했는데, 예상치 못하게 엄청 오래 기다렸다. 열차가 느리기도 하고,
+      끊임 없이 연결되어 있었기에 시간이 정말 길게 느껴졌다.
+    </p>
+    <YoutubeVideo videoId="NlC6BUI-C1k" caption="끊임 없는 캐나다 화물 열차" />
+    <p>
+      캠핑장에 체크인하는 과정에서 직원이 주의를 많이 줬다. 곰은 무서운 걸
+      알테지만, 무스도 곰과 같댄다. 절대로 도발하지 말라며 무스의 뿔에 뜯긴
+      캠핑카 사진을 보여주기도 했다. 마음 깊이 새긴 후, 우리가 예약한 장소로
+      이동했다. 입장하는 순간부터 동물이 많은 것을 느꼈다. 암컷 엘크가 매우 많이
+      있었다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/3MVNhEU.jpeg"]}
+      alt="엘크"
+      caption="주차하자마자 카메라를 꺼내 급하게 찍은 엘크 궁뎅이"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/YqtgjM1.png"]}
+      alt="캠핑장"
+      caption="저 멀리 설산이 보이는 캠핑장"
+    />
+    <p>
+      우리는 다음 날 스카이트램을 타러 이동했다. 캠핑장과 상당히 가까운
+      곳이었기에, 예매한 시간에 맞게 도착할 수 있었다. 스카이트램 탑승 전 기념품
+      샵이 있어 가볍게 쇼핑을 한 다음 우리는 올라갔다. 올라가는 동안 가이드는
+      가벼운 설명과 함께 투어객들의 질문을 받았다. 누군가는 산불이 왜 발생했냐고
+      물었다. 2024년의 재스퍼 산불은 번개로 인해 발생했고, wind shield로 인해
+      번지지 않았다고 했다. 누군가는 저 긴 열차가 도대체 뭘 싣고 다니냐고
+      물었다. 우리가 상상할 수 있는 모든 것을 싣고 다니고 있다고 했다.
+      땅덩어리가 크니까, 택배 트럭 대신하는 것이 아닌가 싶었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/mOJrOV1.png",
+        "https://i.imgur.com/Q6h8owx.png",
+        "https://i.imgur.com/K71vqwv.png",
+      ]}
+      alt="스카이트램"
+      caption="스카이트램 정거장에는 다양한 정보가 있었다."
+    />
+    <p>
+      내리고 보니, 전 날의 폭설로 인해 눈이 가득했다. 등산화를 챙겨오지 못한
+      어머니께서는 중도에 포기할 수 밖에 없을 정도로 눈이 많았다. 등산화를 신은
+      나도 가파른 눈길에서 조금씩 미끄러졌다. 리뷰에는 꽤나 쉬운 코스라고 적혀
+      있었는데, 눈이 쌓인 이 코스는 상당히 어려웠다. 아버지와 함께 끝까지
+      올라갔다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/05m2CAi.jpeg"]}
+      alt="휘슬러 피크"
+      caption="아버지의 모자 덕분에 쉽게 찾을 수 있다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/62Vikud.png"]}
+      alt="눈사람"
+      caption="한국은 보통 2층 눈사람인데, 여긴 3층인가보다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ps8D57Y.png"]}
+      alt="휘슬러 피크"
+      caption="내가 찍었다고 믿을 수 없는 뷰"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/sfxn8yR.png"]}
+      alt="휘슬러 피크"
+      caption="휘슬러 피크에서는 사방이 이런 뷰"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/22vdAel.jpeg"]}
+      alt="휘슬러 피크"
+      caption="밖에서 혼자 기다리시는 어머니"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/iIIEEUf.png"]}
+      alt="휘슬러 피크"
+      height={450}
+      caption="트레일 길이는 1.2km, 수직 상승은 200m 밖에 안되는데 그렇게 힘들었다."
+    />
+    <p>
+      스카이트램을 타고 내려와 온천으로 이동했다. 거리는 그리 멀지 않았지만,
+      마지막 16km 구간이 비포장 도로였기에 속도를 내지 못했다. 온천은 애기들
+      노는 풀장 같은 곳에 온천수를 넣어두고, 몸을 담그는 곳이었다. 적당히 따뜻한
+      정도의 온도였다. 우리 가족은 전 날까지 여행으로 인해 신경이 예민해진
+      상태였는데, 이 온천에서 모든 피로를 싹 풀고 평안해졌다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/XfgXtHM.png"]}
+      alt="차도"
+      caption="온천 가는 길은 그림이었다."
+    />
+    <p>
+      캠핑장으로 돌아오는 길에 절벽에 매달려있는 산양들을 봤는데 아쉽게도 차를
+      멈출 수 없어서 자세히 구경하지 못했다. 동물을 못봤다는 미련으로 인해,
+      캠핑장을 한 바퀴 차로 돌며 여러 동물 친구들을 보면서 하루를 마무리했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/85xmYLn.png",
+        "https://i.imgur.com/1N607U4.png",
+        "https://i.imgur.com/MtGhf3C.jpeg",
+      ]}
+      alt="동물 친구들"
+      caption="캠핑장에 사는 엘크들과 오리들"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/7myvCk4x8TWLktvGA"
+      title="헥토르 호수"
+      description="Hector Lake"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/5YH8dt6AsGBZSVzW7"
+      title="하우스 강 뷰포인트"
+      description="Howse Pass Viewpoint"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/LPRuc8N2y4Na9CDg6"
+      title="브라이들 베일 폭포"
+      description="Bridal Veil Falls"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/Sw4JDfmwBYbmcJHh8"
+      title="애써베스카 빙하"
+      description="Athabasca Glacier"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/nU3Xj3ScydDJyaxa9"
+      title="선왑타 폭포"
+      description="Sunwapta Falls"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/xQp7ESWC6mso7kqW9"
+      title="애써베스카 폭포"
+      description="Athabasca Falls"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/GCzaK4cq65rgrLoC9"
+      title="재스퍼 스카이트램"
+      description="Jasper SkyTram"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/7PDF6R1xNzCEx7d49"
+      title="온천"
+      description="Miette Hot Springs"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/56/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "56",
+  title: "4. 휘슬러 정상에서 바라본 재스퍼",
+  slug: "jasper",
+  summary: "재스퍼에서의 2박 3일",
+  subCategoryId: "9",
+  author: "양가브리엘",
+  publishDate: "2026-07-28 23:43:00",
+  readTime: 5,
+  imageUrl: "https://i.imgur.com/ps8D57Y.png",
+  tags: ["캐나다", "로키산맥", "캠핑카", "재스퍼", "스카이트램", "온천"],
+};
