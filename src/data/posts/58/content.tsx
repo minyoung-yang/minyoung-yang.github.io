a8214b242d30dda31f6e86586f18ca84a56eaf73
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-07-29 23:11:20 +0900

    58번 포스팅

---
+++ src/data/posts/58/content.tsx
@@ -0,0 +1,302 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+import { YoutubeVideo } from "@/components/entry-detail/youtube-video";
+
+export const content = (
+  <>
+    <p>
+      2025년 10월 6일, 우리는 머무르던 휘슬러 캠프그라운드를 떠났다. 로키 산맥의
+      재스퍼 국립공원을 떠나는 날이었다. 서쪽으로 빠지기 전에, 마지막으로 동쪽의
+      멀린 호수로 향했다. 가는 길에 미니 버스와 승용차 몇 대가 서있고, 사람들이
+      어딘가를 바라보고 있었다. 야생동물 구경이라면 신이 나는 나였기에, 바로
+      차를 세웠다. 저 멀리 무스라 불리는 말코손바닥사슴이 있었다. 너무 멀었지만,
+      곰 만큼 보기 어려운 야생 동물이라 촬영과 구경을 하고 다시 이동했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/hgKNgNC.png",
+        "https://i.imgur.com/og69lXI.png",
+      ]}
+      alt="무스"
+      caption="갤럭시 100배 줌의 힘으로 무스를 볼 수 있었다."
+    />
+    <p>
+      조금 더 이동하여 멀린 호수에 도착했다. 비수기가 되는 과도기였는지, 사람이
+      매우 적었다. 또 한 번 광활한 호수를 바라보았다. 호수를 따라 걷기도 했다.
+      누군가의 강아지와 야생 땃쥐, 참새도 볼 수 있었다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/RWC7gle.png"]}
+      alt="멀린 호수"
+      caption="또 다시 광활한 호수"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/bV5GF6V.jpeg"]}
+      alt="멀린 호수"
+      caption="미니 유람선. 날씨가 아쉬웠다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/eq8OY4e.jpeg"]}
+      alt="새"
+      caption="벌레 먹는 새"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/4w4POIt.jpeg"]}
+      alt="땃쥐"
+      caption="처음이자 마지막이 될 것만 같은 땃쥐. 카메라 초점을 못맞췄지만 눈을 흐릿하게 뜨면 보인다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ypsxT2g.jpeg"]}
+      alt="개"
+      caption="좋은 샴푸를 썼을 것만 같은 개"
+    />
+    <p>
+      멀린 호수를 떠나 다음 캠핑장으로 이동했다. 재스퍼 타운을 지나 서쪽으로
+      이동했다. 2024년의 산불로 인해 발가벗은 나무 길을 지나기도 했다. 다행히,
+      어린 나무가 자라는 것이 보였다. 마운트 롭슨 주립공원 웰컴센터를 지나 쭉
+      이동하여 캠핑장에 도착했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/E8l1Ldt.png",
+        "https://i.imgur.com/8K53h9M.jpeg",
+      ]}
+      alt="산불에 탄 길"
+      caption="불에 탄 나무를 보면 마음이 아프긴 하지만, 이 또한 자연의 섭리다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/DIZlYwj.png"]}
+      alt="마운트 롭슨 주립공원"
+      caption="마운트 롭슨 주립공원 웰컴센터는 배산임수 지형이다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/BlLm8df.png"]}
+      alt="마운트 롭슨 주립공원"
+      caption="저게 롭슨 산인가"
+    />
+    <p>
+      전 날까지는 국립공원에서 운영하는 캠핑장이었고, 이 날부터 사설 캠핑장에서
+      숙박하게 되었다. 첫 사설 캠핑장이었는데, 상당히 정감 넘치는 곳이었다. 북미
+      시골 청년이, 키우는 강아지와 함께 우릴 맞이해줬다. 그 동안 머물렀던
+      캠핑장보다 숙박객이 훨씬 적었고, 사이트 간 간격도 훨씬 넓었다. 캐나다 여행
+      중 머물렀던 캠핑장 중 베스트라고 생각이 들었던 곳이다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/ic6JAq9.png"]}
+      alt="캠핑장 뷰"
+      caption="캠핑장에서 바라보는 뷰"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/DKUOpb8.jpeg"]}
+      alt="카누 강"
+      caption="캠핑장 앞에는 카누 강이 흐른다. 그래서 카누 강 캠핑장이다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/AxKT5rC.jpeg"]}
+      alt="다람쥐"
+      caption="람쥐가 우릴 맞이해줬다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/NRqgfAx.png"]}
+      alt="달"
+      caption="이 날 밤은 달이 매우 밝았다. 갤럭시로 찍은 보름달."
+    />
+    <p>
+      다음 날, 체크아웃하며 인사하고 떠나려 했는데, 문이 잠겨있었다. 좋은
+      캠핑장이었다고 말하려 했는데, 아쉬웠다. 우린 길을 떠나 웰스 그레이
+      주립공원으로 향했다. 웰스 그레이 주립공원에는 큰 폭포가 세 개 있었는데, 각
+      폭포마다 보는 맛이 달랐다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/cCsWLU2.png"]}
+      alt="폭포 안내판"
+      caption="웰스 그레이 주립공원 내의 폭포를 그림으로 설명한 안내판"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/nBa4rgo.jpeg"]}
+      alt="폭포"
+      caption="Spahats 폭포. 비밀스러운 곳에서 시원하게 흐르고 있다."
+    />
+    <YoutubeVideo
+      videoId="wExiCCDQirc"
+      caption="Spahats 폭포는 절제미가 있다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/GXE4Q4b.png"]}
+      alt="Dawson 폭포"
+      caption="Dawson 폭포. 20m 높이라는데 그 정도인지는 잘 모르겠다."
+    />
+    <YoutubeVideo
+      videoId="RTiLTEMxOs0"
+      caption="Dawson 폭포는 넓고 시원하다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/FOPUv5C.png"]}
+      alt="헬름켄 폭포"
+      caption="헬름켄 폭포. 압도된다."
+    />
+    <YoutubeVideo
+      videoId="mZ3qUZQXCLc"
+      caption="헬름켄 폭포는 엄청나다. 경외감이 든다."
+    />
+    <p>
+      삼폭포를 본 뒤, 목적지로 삼았던 캠핑장으로 이동했다. 방문해보니, 지금까지
+      머물렀던 캠핑장과는 다르게 전기나 수도 시설이 없었다. 아직 낮 시간대이기도
+      하여, 우리는 더 이동하여 다른 캠핑장으로 이동하기로 했다. 웰스 그레이
+      주립공원 입구에 클리어워터라는 마을이 있었는데, 꽤 괜찮아 보이는 캠핑장을
+      발견하여 머무르기로 했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/4GzF1TO.png",
+        "https://i.imgur.com/cgdBBdH.jpeg",
+      ]}
+      alt="캠핑장"
+      caption="작은 호수가 있었다. 사과 나무도 있어서 먹어봤는데 꽤 맛이 괜찮았다."
+    />
+    <p>
+      다음 날 우리는 차로 330km를 이동했다. 중간에 구경할만한 포인트가 별로
+      없었기에 계속 운전하는 것이 가능했던 거 같다. 일부러 조금 늦은 시간에
+      출발하여, 여유있게 이동했다. 중간에 캠룹스라는 도시에서 팀홀튼 커피도
+      마시고, 리얼 캐네디언 슈퍼스토어도 방문하여 장을 봤다. 그 외에는 정말 아무
+      것도 안했다. 기록을 보니, 작은 휴게소에서 잠시 쉬긴 했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/fCMOBpr.png",
+        "https://i.imgur.com/G9LT2E9.png",
+      ]}
+      alt="리얼 캐네디언 슈퍼스토어"
+      caption="팀홀튼 커피를 마시며 리얼 캐네디언 슈퍼스토어를 방문하는 한국인"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/oMM3z63.jpeg"]}
+      alt="휴게소 트럭"
+      caption="휴게소에는 감성 넘치는 푸드 트럭 (?)이 있었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/zu3ZUnH.jpeg"]}
+      alt="휴게소"
+      caption="텅빈 곳에 주차할 수 있었던 휴게소"
+    />
+    <p>
+      장기 투숙객이 많아 보이는 캠핑장에서 하루를 마무리했다. 캠핑 카를 집으로
+      삼아 캠핑장에서 월세로 살아가는 사람들이 많은 듯 했다. 차도에 붙어있는
+      캠핑장이었지만, 입구로 들어가는 순간부터는 숲속 세계에 간 듯한 마법적인
+      캠핑장이었다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/ytr4guz.jpeg"]}
+      alt="캠핑장"
+      caption="우리가 하루 머물렀던 공간"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/j9sgBFJ.jpeg"]}
+      alt="오피스"
+      caption="캠핑장 오피스"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/IiULpRn.jpeg"]}
+      alt="캠핑장 앞 차도"
+      caption="구글 맵 상으로 주변에 트레일이 있어서 가보려 했지만, 걸어서 갈 방법이 없어 보여 다시 돌아왔다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/Xyx0MJR.jpeg"]}
+      alt="파랑새"
+      caption="캠핑장에서 발견한 삐리삐리 파랑새"
+    />
+    <YoutubeVideo videoId="NFiy8dg5eY4" caption="따뜻해보이는 캐나다 송충이" />
+    <p>
+      다음 날 밴쿠버 아일랜드로 들어가는 배편을 예매해야 했는데, 너무 여유롭게
+      생각하여 원하는 시간 예약에 실패했다. 그래서 계획보다 늦은 오후 5시로
+      예매했다. 그 사이 뭘 하면 좋을지 찾아오라는 대장님의 명령에, 동물을
+      구경하고 싶은 생각에 근방 철새 도래지로 안내했다. 약간의 입장료를 내고
+      산책하며 새들과 놀았다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/4zaJT9y.jpeg"]}
+      alt="오리"
+      caption="혼자 있는 오리를 바라보는 아버지를 찍으시는 어머니를 찍은 나"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/wpWezYy.png"]}
+      alt="새"
+      caption="기지개 피는 새"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/F0qIjrL.png"]}
+      alt="들판"
+      caption="허수아비를 참 대충 만들어놨다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/C6KEFiq.png"]}
+      alt="새"
+      caption="이 새의 이름은 red-winged blackbird이다. 말 그대로 빨간 날개를 가진 검은 새다."
+    />
+    <YoutubeVideo videoId="aGdBTrc59rw" caption="날 엄마로 삼은 새끼 오리" />
+    <p>
+      늦지 않게 항구로 이동하여, 배를 탔다. 이렇게 큰 차가 실리는 것이
+      신기했지만, 우리 차보다 10배는 무거울 법한 트럭도 꽤 많았다. 바닷바람을
+      쐬며 밴쿠버 아일랜드로 이동했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/TCkBJZk.png",
+        "https://i.imgur.com/yoqs0i2.png",
+      ]}
+      alt="배"
+      caption="날씨가 환상적이었던 밴쿠버 아일랜드 행 배"
+    />
+    <YoutubeVideo videoId="4x5n0UZ3Itk" caption="잔잔하지만 빠르다." />
+    <OgLink
+      href="https://maps.app.goo.gl/h9afRqKin8v4CXFc8"
+      title="시골 청년의 캠핑장"
+      description="Canoe River Campground"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/EBj9RCDo6BnTJtgx8"
+      title="Spahats 폭포"
+      description="Spahats Creek Falls"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/WQ18hWXeLUcCiRMY9"
+      title="도슨 폭포"
+      description="Dawson Falls"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/uLxVW9dG8tXkQr7S9"
+      title="헬름켄 폭포"
+      description="Helmcken Falls"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/Zvmaiq9AHeAUj9Cy6"
+      title="사과나무가 있던 캠핑장"
+      description="Dutch Lake Resort & RV Park"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/GYGAFRCsBoPQ11UK7"
+      title="찐 캐나다인만 간다는 대형마트"
+      description="Real Canadian Superstore Columbia Street"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/Hhqi9cg1H3fShKJp8"
+      title="도로 앞의 캠핑장"
+      description="WildRose Campground & RV Park"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/WMHhh1Vt2UaygstQ6"
+      title="철새 도래지"
+      description="George C. Reifel Migratory Bird Sanctuary"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/58/meta.tsx
@@ -0,0 +1,21 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "58",
+  title: "5. 로키 산맥을 떠나 밴쿠버 아일랜드로",
+  slug: "to-vancouver-island-from-rocky-mountains",
+  summary: "로키 산맥을 떠나 밴쿠버 아일랜드로 이동하는 여정",
+  subCategoryId: "9",
+  author: "양가브리엘",
+  publishDate: "2026-07-29 23:10:00",
+  readTime: 7,
+  imageUrl: "https://i.imgur.com/FOPUv5C.png",
+  tags: [
+    "캐나다",
+    "로키산맥",
+    "밴쿠버 아일랜드",
+    "캠핑",
+    "철새 도래지",
+    "폭포",
+  ],
+};
