fbbd2d81df819bb948a9f7472c8682fefc766c69
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-08-01 01:00:01 +0900

    59번 포스팅

---
+++ src/data/posts/59/content.tsx
@@ -0,0 +1,329 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+
+export const content = (
+  <>
+    <p>
+      스와츠베이 항구를 통해 빅토리아 아일랜드에 입성했다. 거의 저녁 7시가 다
+      되어 하선했기에 길이 어두워졌다. 직전에 알아둔 캠핑장으로 한 시간 정도
+      운전하는데, 무서울 정도였다. 빅토리아는 브리티시 컬럼비아 주의 주도여서
+      그런지 차량이 많았는데, 그래서 더 무서웠다. 캠핑카는 우리 뿐이었고, 모두
+      쌩쌩 달리며, 또 캐나다는 왜 하이빔을 켜고 달리는지. 그렇게 겨우 겨우
+      캠핑장에 도착했다. 오피스에서 체크인할 때 주의를 들었다. 밤에 곰이 나올 수
+      있으니 조심하라고.
+    </p>
+    <p>
+      아침에 일어나서 캠핑장을 둘러보니, 엄청난 곳이었다. 앞에는 수크 강이
+      흐르는 숲이었는데, 온갖 동물이 돌아다니는 곳이었다. 아쉽게도 곰은 없었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/OsMlBGz.png",
+        "https://i.imgur.com/oCi1AWY.png",
+      ]}
+      alt="물범"
+      caption="자네는 물범 같고만."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/lMkPt05.jpeg"]}
+      alt="새"
+      caption="갈매기보다 작고, 참새보다 큰 어떤 새"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/bNYW96p.jpeg"]}
+      alt="거위"
+      caption="거위가 맞나?"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/t2dMDqF.jpeg"]}
+      alt="개"
+      caption="개도 있다."
+    />
+    <p>
+      이제 어디 갈지를 고민했다. 왜냐면, 우리는 밴쿠버 아일랜드에 예정보다 하루
+      일찍 왔기 때문이다. 메인랜드에서 갈 곳이 없다고 판단하였기에, 하루 먼저
+      갔다. 그래서 우리는 아침 식사를 하며, 어디를 들를지 찾아봤다. 아버지께서
+      미리 찾아본 로얄 로드 대학에 방문하기로 했다. 빅토리아의 서쪽에 위치한
+      캠퍼스였는데, 여러 컨셉의 정원을 무료로 개방해두었기에 방문하기 좋았다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/q1Ej79I.png"]}
+      alt="공작새"
+      height={450}
+      caption="캠퍼스에 공작새가 돌아다닌다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/UrdBboU.jpeg",
+        "https://i.imgur.com/SjwcIsg.png",
+      ]}
+      alt="동물"
+      caption="정원은 못찍고 동물만 찍었다."
+    />
+    <p>
+      로얄 로드 대학 근방에 방파제 같은 곳이 있어 바다를 볼 수 있었다. 차를 끌고
+      이동하여 비오는 바닷가를 봤다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/yIH6ixx.png"]}
+      alt="바다"
+      caption="바다를 바라보는 아버지. 타임랩스를 찍고 계신다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/A21fLrA.png"]}
+      alt="바다"
+      caption="바다를 바라보는 어머니."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/5gd26XS.jpeg"]}
+      alt="바다"
+      caption="바다를 바라보는 갈매기."
+    />
+    <p>
+      일정을 마무리할 겸, 다음 캠핑장으로 이동했다. 이 캠핑장은 여행 두 달
+      전부터 알아본 곳이었다. 아버지께서 꽤 괜찮다고 생각하셨는데, 예약이 어려워
+      나에게 맡기셨다. 나도 접속이 어려운 페이지였는데, 알고 보니 특정
+      국가에서는 접속이 안되는거 같았다. 여러 VPN 서비스를 다운로드 받아서
+      시도하다가 성공한 시도에 예약을 완료했다. 캐나다 여행 중 숙박한 캠핑장 중
+      금액이 가장 비쌌다. 직접 방문해보니, 메인 스트릿과의 거리, 시설 등 모두
+      비싼 값어치를 했다. 샤워실과 화장실도 카드키를 통해 열리는 형태였다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/YI5Y7sA.png"]}
+      alt="바다"
+      caption="캠핑장 앞에는 관광용 경비행기가 자주 이착륙한다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/U322ENT.png"]}
+      alt="캠핑장"
+      caption="시설이 고급스러운 것에 비해 차량 간 거리는 좁은 편이었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/dadXDLt.png"]}
+      alt="바다"
+      caption="블랙볼 항구로 들어가는 여객선. 시애틀이나 포트앤젤레스에서 온 배다."
+    />
+    <p>
+      숙소를 나와 동네 마실을 돌며 일정을 마무리했다. 조금 걷다보니 서브웨이가
+      있어서 길게 한 줄 포장해왔다. 저녁 요리 부담이 줄었을거다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/Al88NyE.jpeg"]}
+      alt="집"
+      caption="한적한 집 사이 길을 걷다보면 작은 상가가 나온다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/JhnMBV0.jpeg"]}
+      alt="마을"
+      caption="음악 학원인가. 소공연장인가."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/hIiVMSO.png"]}
+      alt="타투가게"
+      caption="간판이 없어도 타투 샵이라는 것을 알 것만 같다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/wcgOmU9.png",
+        "https://i.imgur.com/EdS7xRj.png",
+      ]}
+      alt="맥주"
+      caption="일정이 빨리 끝난 날에는 맥주로 시간을 보낸다."
+    />
+    <p>
+      다음 날 우리 가족은 빅토리아를 관광했다. 캠핑장에서 조금 걸어 나가 버스를
+      타고 20분 정도 이동했다. 역시 도시가 커지면 어렵다. 버스에는 노숙자가 함께
+      탑승하여 숨을 쉬기 어려웠다. 빅토리아에 도착하여 내린 후에는 혹시 모를
+      위험에 대비하여 가방을 꽉 잡고 걸었다. 먼저 비콘힐 공원에 가서 또 다시
+      자연을 느껴줬다. 관리도 잘 되고, 볼 것도 많은 공원이었다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/ewwzVQR.jpeg"]}
+      alt="나무"
+      caption="추욱 처진 느티나무"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/totqf7x.jpeg"]}
+      alt="공원"
+      caption="속도가 빠른 아버지와 뒤따라가는 어머니"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/8hmbpFp.png"]}
+      alt="바다"
+      caption="이번 여행에서 찍은 사진 중 가장 좋아하는 사진이다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/tIMywfT.png"]}
+      alt="언덕"
+      caption="어머니는 내려오지 못하셨다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/6eHRIaG.jpeg"]}
+      alt="청설모"
+      caption="야생의 탐리스가 나타났다."
+    />
+    <p>
+      브리티시 컬럼비아 주의 의사당도 구경하고 캠핑장으로 돌아왔다. 오랜만에
+      사람 많은 곳에 가고, 비도 왔기에 체력이 급하게 소진되었다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/BVRYxD0.png"]}
+      alt="의사당"
+      caption="카메라에 담기 어려운 웅장함이 있었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/xeUd8oy.png"]}
+      alt="의사당"
+      caption="625 전쟁에 참전한 캐나다 군 동상. 감사합니다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/msbP42t.png"]}
+      alt="호텔"
+      caption="의사당 앞의 호텔"
+    />
+    <p>
+      다음 날 우리는 이 비싼 캠핑장을 떠나기 전에 온 가족이 샤워실을 이용했다.
+      마지막으로 캠핑장에서의 사진을 찍고 나나이모로 향했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/MsMcuWq.png"]}
+      alt="개"
+      caption="잠이 덜 깬 멍멍이"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/fb0awFV.png"]}
+      alt="새"
+      caption="바다 위의 이정표이려나"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/c0RzGHg.png"]}
+      alt="캠핑장"
+      caption="비행기가 날아다니는 캠핑장"
+    />
+    <p>
+      나나이모까지는 100km가 좀 넘었다. 가는 길에, 아버지가 미리 알아보셨다는
+      카페에 들러 아이스크림을 사먹었다. 상당히 맛이 좋았는데, 아버진 어떻게
+      이걸 찾으셨는지. 나나이모에 도착하여 해안가를 걸었다. 비가 와서 그런지
+      사람이 상당히 적었고, 기념품 샵도 많이 닫혀있었다. 오히려 좋아. 천천히
+      걸으며 나나이모의 분위기를 느꼈다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/rdEYlf6.png",
+        "https://i.imgur.com/PYftufL.png",
+      ]}
+      alt="아이스크림"
+      caption="도라에몽 손과 조수석의 멍멍이"
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/x1a8RzD.png",
+        "https://i.imgur.com/FRf17ci.png",
+      ]}
+      alt="갈매기"
+      caption="갈매기는 외롭다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/CsNrayN.png",
+        "https://i.imgur.com/UbqO9zM.png",
+      ]}
+      alt="조각상"
+      caption="해안가를 따라 다양한 조각상이 있었다. 원주민, 개척자 모두."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/yMM2ogw.png",
+        "https://i.imgur.com/quC1hc3.png",
+      ]}
+      alt="구조물"
+      caption="다양한 색이 있는 나나이모"
+    />
+    <p>
+      가벼운 관광을 마치고, 하루 묵을 나나이모의 캠핑장으로 이동했다. 지금까지
+      머물렀던 캠핑장 중 규모가 가장 컸으며, 리셉션에서 캠핑장 지도를 주며 내가
+      머물 장소를 알려줄 정도였다. 캠핑장 안에 뷰포인트가 2~3개 있을 정도였다.
+      대가족이 휴가를 즐기러 오는 숲 속이었다. 차를 정박하고, 캠핑장을
+      돌아다녔다. 밴쿠버 아일랜드의 마지막 날 밤을 보내고, 메인랜드에서의
+      관광만을 남기고 있다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/xGylAui.jpeg"]}
+      alt="캠핑장"
+      caption="숲 속에 정박한 우리 캠핑카"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/pb6Xixd.png"]}
+      alt="돌탑"
+      height={500}
+      caption="본드로 붙인건가"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/qRhD9mX.jpeg"]}
+      alt="강아지"
+      caption="뷰포인트에서 날 노려보던 강아지"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/5rjElun.png"]}
+      alt="바다"
+      caption="내가 제일 좋아하는 사진은 이거로 할란다. 바다와 배와 산"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/vKq50BW.jpeg"]}
+      alt="토끼"
+      caption="야생 토끼가 돌아다닌다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/3eeu10i.png",
+        "https://i.imgur.com/3sBRsfh.png",
+      ]}
+      alt="불과 맥주"
+      caption="밴쿠버 아일랜드의 마지막 밤은 불과 맥주로."
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/7wxwUn29cZ6nnDEj6"
+      title="곰 나오는 캠핑장"
+      description="Sooke River Campground"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/N5LEsd7R7sRHrRLq9"
+      title="정원이 조성된 대학"
+      description="Royal Roads University"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/tTG2a781nh6HN3cD9"
+      title="비싸지만 시설 좋은 캠핑장"
+      description="Salish Seaside RV Haven"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/7snMoqm4HWHPTWTD7"
+      title="빅토리아의 공원"
+      description="Beacon Hill Park"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/BBDK2QKr2612WgBV7"
+      title="브리티시 컬럼비아 주 의사당"
+      description="Legislative Assembly of British Columbia"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/vWKN3a4Lthok2Fpu9"
+      title="아이스크림 맛있는 카페"
+      description="The Old Farm Market Coffee Shop"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/P8rC7TzZhrNcz8CP9"
+      title="숲 속 캠핑장"
+      description="Living Forest Oceanside Campground & RV Park"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/59/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "59",
+  title: "6. 비오는 밴쿠버 아일랜드",
+  slug: "rainy-vancouver-island",
+  summary: "비오는 밴쿠버 아일랜드에서의 여정 (빅토리아와 나나이모)",
+  subCategoryId: "9",
+  author: "양가브리엘",
+  publishDate: "2026-08-01 01:04:00",
+  readTime: 7,
+  imageUrl: "https://i.imgur.com/5rjElun.png",
+  tags: ["캐나다", "밴쿠버 아일랜드", "캠핑", "빅토리아", "나나이모"],
+};
