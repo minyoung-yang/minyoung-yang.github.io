7ca38a2c8a33ee2664cfa6c502246424035aa8cf
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-08-02 01:08:54 +0900

    60번 포스팅

---
+++ src/data/posts/60/content.tsx
@@ -0,0 +1,178 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+import { YoutubeVideo } from "@/components/entry-detail/youtube-video";
+
+export const content = (
+  <>
+    <p>
+      2025년 10월 13일, 오전에 Duke Point로 이동하여 배에 차를 실었다. 밴쿠버
+      아일랜드로 향할 때 출발했던 Tsawwassen에 도착했다. 다음 숙소 체크인까지
+      시간이 남았기에 잠시 어딜 들를까 고민하던 와중에, 목적지는 아울렛으로
+      정했다. 밴쿠버 아울렛이 저렴하지 않다는 이야기를 듣고 안 가려 했는데, 마침
+      시간이 남아버렸다. 하지만 비교적 저렴한 브랜드도 있었다. 노스페이스,
+      랄프로렌, 코치가 저렴하여 와이프, 친척 등의 선물을 구입했다. 가볍게 쇼핑을
+      마친 뒤, 숙소 체크인 후 숙소 옆 식당에서 저녁 식사 후 코스트코에서 장을 본
+      뒤 하루를 마무리했다. 참고로 호텔이었고, 지난 숙소를 마지막으로 캠핑장은
+      가지 않았다. 호텔은 저렴했고 그 값에 맞는 청결도와 조식이 느껴졌다.
+    </p>
+    <YoutubeVideo
+      videoId="gcgKPxHH3zk"
+      caption="메인랜드로 출항하는 길에 아버지가 찍으신 타임랩스."
+    />
+    <p>
+      다음 날 오전, 정들었던 캠핑카를 반납하러 갔다. 직원이 각종 도구와 시설을
+      살펴보더니, 아무 이슈 없다고 알려줬다. 싸인 후 반납 과정을 마쳤다. 반납
+      대기부터 끝까지 20분 정도 걸렸지만, 다시 밴쿠버까지 이동하는 셔틀 버스를
+      탑승하려니 90분을 대기하라고 한다. 시내 버스나 우버로 이동하려니 가성비가
+      나지 않아 90분을 대기했다. 셔틀 버스는 다른 손님들을 위해 공항을 들렀다가
+      워터프론트로 이동했다. 우리는 블루 호라이즌 호텔이라는 곳에 내려 밴쿠버
+      관광을 시작했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/JM8vaYS.png"]}
+      alt="캠핑카"
+      height={400}
+      caption="정들었던 캠핑카. 처음엔 좁고 불편했지만, 부모님과의 추억이 담긴 2주 간의 집이었다."
+    />
+    <p>
+      우리의 코스는 간단했다. 스탠리 공원 남동쪽을 걷고, 워터프론트의 명소를 한
+      두 군데 보고 숙소로 돌아가는 것이었다. 날씨는 여행 일정 중 최고였고,
+      걷기를 시작했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/DFY09BY.jpeg"]}
+      alt="워터프론트"
+      caption="워터프론트에서 본 첫 장면이다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/KUzx1QD.jpeg"]}
+      alt="경비행기"
+      caption="여기도 경비행기가 관광 코스인가보다."
+    />
+    <YoutubeVideo
+      videoId="1FyhmPJlaXQ"
+      caption="비행기가 금방 뜰 줄 알았는데, 꽤 늦게 떴다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/8xxX3A4.jpeg",
+        "https://i.imgur.com/eSYBs7l.png",
+      ]}
+      alt="가마우지"
+      caption="가마우지가 모여있던 시멘트 섬"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/fegHRYW.png"]}
+      alt="집"
+      caption="단순 모형인가. 오피스인가. 사다리는 어디 있는가."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/RnyKJ0b.png"]}
+      alt="밴쿠버"
+      caption="스탠리 공원으로 가는 다리에서 본 밴쿠버 빌딩 숲"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/rNWOigG.png"]}
+      alt="기념품샵"
+      caption="기념품 샵이 보이면 발걸음이 멈추는 어머니와 나는 같은 유전자다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/FC0LoP4.jpeg"]}
+      alt="토템폴"
+      caption="밴쿠버 원주민의 흔적인 토템폴"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/dJlyRUz.png"]}
+      alt="공장"
+      caption="선박 물류 창고 같다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/yN9bRMO.jpeg"]}
+      alt="개발자국"
+      caption="어떻게 걸으면 발자국이 저렇게 나올까"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ri0nNQ7.png"]}
+      alt="해녀상"
+      caption="인어가 아니라 해녀다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/yV1cMCA.jpeg"]}
+      alt="바다"
+      caption="어머니와 나"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/Zexx4G6.png"]}
+      alt="오리"
+      caption="스탠리 공원 내 작은 호수에 오리가 우아하게 헤엄친다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/IHTM6Pl.jpeg"]}
+      alt="나무"
+      caption="이 쓰러진 나무가 몇 살인지 세보시는 아버지"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/uqVPMoO.png"]}
+      alt="나무"
+      caption="다 세고 보니, 누군가가 세 놓은 흔적이 있었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/KgCdqLJ.jpeg"]}
+      alt="나무"
+      caption="죽은 나무 위에 또 다른 나무가 자라난다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/6oa23X9.jpeg"]}
+      alt="증기시계"
+      height={400}
+      caption="워터프론트의 상징인 증기시계. 사실 별거 없다."
+    />
+    <p>
+      기록을 보니, 네 시간을 넘게 돌아다녔다. 마지막 날이라는 생각에 열심히
+      돌아다녔다. 우린 다음 날 호텔의 셔틀버스를 타고 밴쿠버 공항으로 이동했다.
+      캘거리 공항을 경유해 인천 국제 공항으로 돌아왔다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/lKAd4Hj.png",
+        "https://i.imgur.com/aS7MqCF.png",
+      ]}
+      alt="공항 및 비행기"
+      caption="면세점에서 아이스와인을 샀다. 매우 달다. 술 같지 않다."
+    />
+    <p>
+      부모님과 여행을 갈 때 항상 마지막일 수도 있다는 생각으로 간다. 물론 한참
+      더 오래 건강하시겠지만, 이런 장거리/장기간 여행은 곧 쉽지 않아질 것이다.
+      칠순이 넘은 아버지와 올해 지공족이 된 어머니시다. 이번 여행처럼 조만간 또
+      모시고 어디론가 가보고 싶다. 또 다시 캠핑카를 타고 대자연 속을 탐험하고
+      싶다. 그만큼 이 여행이 행복하고 좋은 기억으로 남아있다. 다시 한 번
+      부모님과 여행을 떠나고, 그 여행을 남길 수 있기를 바라며.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/SoWBVqO.png"]}
+      alt="마그넷"
+      height={500}
+      caption="이번 여행에서 구한 마그넷들"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/6LkkKLxG69SwEbdm6"
+      title="아울렛"
+      description="McArthurGlen Designer Outlet Vancouver Airport"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/7PPaCypgFywtF7Dc6"
+      title="스탠리 공원"
+      description="Stanley Park"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/f9mdk4P6GohcDmF17"
+      title="저렴한 호텔"
+      description="Abercorn Hotel, Trademark Collection by Wyndham"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/60/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "60",
+  title: "7. 캐나다 여행의 마지막 일정을 밴쿠버에서",
+  slug: "last-day-in-vancouver",
+  summary: "밴쿠버에서의 2박 3일",
+  subCategoryId: "9",
+  author: "양가브리엘",
+  publishDate: "2026-08-02 01:07:00",
+  readTime: 7,
+  imageUrl: "https://i.imgur.com/ri0nNQ7.png",
+  tags: ["캐나다", "밴쿠버", "스탠리공원", "워터프론트"],
+};
