0e7b90d5438e4cb6e82e05c5fca2498a6eac72ec
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-08-03 01:11:26 +0900

    61번 포스팅

---
+++ src/data/posts/61/meta.tsx
@@ -0,0 +1,14 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "61",
+  title: "2-0. 미국 신혼여행 계획",
+  slug: "honeymoon-america-plan",
+  summary: "미국으로의 신혼여행을 계획하다.",
+  subCategoryId: "4",
+  author: "양가브리엘",
+  publishDate: "2026-08-02 01:07:00",
+  readTime: 3,
+  imageUrl: "https://i.imgur.com/0kNGE7J.png",
+  tags: ["미국", "신혼여행", "시애틀", "샌프란시스코", "국립공원"],
+};
