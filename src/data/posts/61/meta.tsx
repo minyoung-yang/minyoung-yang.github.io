0538831cc1aa66c478fe5762bec2e2d8eb2973af
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-08-03 12:07:41 +0900

    62번 포스팅 작성 날짜 변경

--- src/data/posts/61/meta.tsx
+++ src/data/posts/61/meta.tsx
@@ -7,7 +7,7 @@ export const postMeta: PostMeta = {
   summary: "미국으로의 신혼여행을 계획하다.",
   subCategoryId: "4",
   author: "양가브리엘",
-  publishDate: "2026-08-02 01:07:00",
+  publishDate: "2026-08-03 01:07:00",
   readTime: 3,
   imageUrl: "https://i.imgur.com/0kNGE7J.png",
   tags: ["미국", "신혼여행", "시애틀", "샌프란시스코", "국립공원"],
