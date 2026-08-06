00b4b3239a7a93195d38f3e1c107b0b9db5e3fc1
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-08-07 01:23:55 +0900

    63번 포스팅

---
+++ src/data/posts/63/content.tsx
@@ -0,0 +1,288 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+import { YoutubeVideo } from "@/components/entry-detail/youtube-video";
+
+export const content = (
+  <>
+    <p>
+      2026년 2월 1일, 시애틀에서 배를 타고 베인브릿지 아일랜드에 내렸다. 70분
+      정도를 차로 이동하여 Sequim에 있는 코스트코로 이동했다. 한국에서 코스트코
+      멤버십에 가입할 때, 글로벌 데이터 이동에 대한 동의를 하면 전세계
+      코스트코에서 쇼핑이 가능하다. 특히나 코스트코가 많은 북미에서는 여행
+      준비물이나 기념품도 살 수 있고, 끼니를 때우거나, 저렴하게 주유할 수도
+      있다. 우린 여행 중 군것질할 초코바와 바나나, 물을 구입했다. 그리고 이 날은
+      저녁 식사할 식당이 없었기에, 피자 한 판을 구입했다. 스노우 체인도 구입하려
+      했는데, 없어서 바로 앞에 있는 월마트로 이동했다. 올림픽이나 크레이터
+      레이크, 요세미티 국립공원에서는 겨울 시즌 일부 구간에서 스노우 체인을 갖고
+      있어야 한다는 법규가 있다. 검사를 당해본 적은 없지만, 혹시 모를 상황에
+      대비하여 여행 초기에 구입하는 것을 추천한다. 또한, 월마트의 경우 다른
+      지점에서 환불이 가능하기 때문에 구입 후 한 번도 사용하지 않았다면 한국으로
+      돌아오기 전 환불 받을 수 있다.
+    </p>
+    <p>
+      살 것들을 구입한 후, 사파리에 방문했다. 관광객이 직접 본인의 차를 몰고
+      동물원을 돌아다니는 관광지이다. 많은 사람이 방문하는 필수 코스는 아니지만,
+      우리 부부가 동물 구경을 좋아하기도 하며, 내 차로 직접 (렌터카로 책임 없이)
+      사파리를 즐길 수 있다는 것이 흥미로웠기에 방문했다. 입장료는 1인당
+      25불이었다. 차량으로 들어가는데, 차량 당 가격이 아니었다. 그리고
+      동물들에게 줄 수 있는 통밀빵도 판매하는데, 5달러에 사람이 먹는 식빵 3통
+      정도 이어 붙인 크기였다. 신박한 경험이었다. 일본 나라 공원의 사슴처럼 빵
+      달라고 달려드는 동물이 매우 많았다. 다음 번에 올림픽 국립공원을 갈 일이
+      있다면, 무조건 다시 방문하고 싶은 곳이다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/Et2ugKX.png"]}
+      alt="라마"
+      caption="빵 달라고 달려드는 라마. 무서워서 창문을 닫았다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/nws91M3.jpeg"]}
+      alt="라마"
+      caption="주변에 먹을 것이 충분하다면, 밥 달라고 달려들지 않는다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ufpoIGP.png"]}
+      alt="독수리"
+      caption="독수리도 있었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/U7UUX6u.png"]}
+      alt="곰"
+      caption="곰도 있었다."
+    />
+    <YoutubeVideo
+      videoId="jFkLu3iTVA8"
+      caption="우리가 너무 시끄러워서 소리는 날렸다."
+    />
+    <p>
+      사파리를 마치고 굿즈샵에서 마그넷을 구입하고, 본격적인 올림픽 국립공원
+      탐방에 나섰다. 허리케인 릿지에 가기 위해, 비지터 센터에 방문했다. 올림픽
+      국립공원의 굿즈를 이것 저것 구경 후 구입한 후, 직원에게 허리케인 릿지에
+      올라가도 되냐고 물어봤다. 스노우 체인을 갖고 올라가라며 CCTV를 보여주었다.
+      CCTV는 하얀 백지이길래, 뭐냐고 물어보니 눈이 오고 있어서 그렇다고 한다.
+      비지터센터 밖에서는 전혀 눈이 안 오고 있어서, 직원이 이상한 소리를 한다고
+      생각하고 올라갔다. 올라가는 길에 뷰포인트에서 올림픽 국립공원의 산맥 뷰를
+      볼 수 있었다. 그리고 허리케인 릿지에 거의 도착할 때 쯤부터 눈보라가 쳤다.
+      허리케인 릿지는 뷰가 좋기로 유명한 곳인데 세상이 하얗게 아무것도 안
+      보였다. 국립공원 직원의 말이 맞았다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/hccbuHO.png"]}
+      alt="산"
+      caption="올라가는 길에서의 뷰포인트. 날씨가 살짝 아쉽다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/o66JP2j.png",
+        "https://i.imgur.com/iYpqYCG.png",
+        "https://i.imgur.com/rJhhXzv.png",
+      ]}
+      alt="허리케인 릿지"
+      caption="허리케인 릿지에 있던 비지터 센터는 2023년 화재로 인해 소실되었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/8WJGNuX.png"]}
+      alt="산"
+      height={450}
+      caption="내려가는 길에 들른 또 다른 뷰포인트"
+    />
+    <p>
+      허리케인 릿지에서 내려와 크레센트 호수로 이동했다. 원래 꽤나 많은 계획을
+      세웠었지만, 안 좋은 날씨와 고된 운전으로 인하여 많은 것을 생략하고 호수
+      주변을 산책했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/FpevSmc.png",
+        "https://i.imgur.com/tfLob3O.png",
+      ]}
+      alt="호수"
+      caption="많이 피곤했는지, 크레센트 호수에 방문한 기억이 없다. 사진만으로 기억할 뿐."
+    />
+    <p>
+      호수에서 바로 숙소로 이동했다. 제일 기대했던 숙소였는데, 아쉽게도 날씨가
+      좋지 않아 온전히 느끼진 못했다. 비수기라서 손님도 우리 뿐이었다.
+      호텔이라기보다 B&B 느낌이었는데, 조식은 우리가 미리 주문한 메뉴를 관리인이
+      직접 해주는 형태였다. 주방에서의 소리를 듣고 깨어나, 씻고 조식을 먹었다.
+      특별히 맛있는 것은 아니었지만, 누가 하는지도 모르는 호텔 조식 뷔페를
+      먹다가, 우리가 직접 고른 메뉴를 관리인이 해준다는 생각이 들어서 더 맛있게
+      먹었던 거 같다. 더 필요한게 없냐는 말에, 가는 길에 마실 수 있도록 커피를
+      더 달라고 했다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/3ALIoKB.png",
+        "https://i.imgur.com/URy3n61.png",
+        "https://i.imgur.com/5koDuYK.png",
+      ]}
+      alt="숙소"
+      caption="산 속의 숙소에서 대접받았다."
+    />
+    <p>
+      올림픽 국립공원에서 기대하는 장소 중 하나였던 리알토 비치로 이동했다.
+      날씨가 안 좋아서 상당히 아쉬웠다. 신비로운 분위기가 느껴져야 하는데,
+      지저분한 분위기로 느껴졌다. 바다에서 떠내려온 나무가 바다 쓰레기로
+      느껴졌다. 정말 기대하는 장소였는데 많이 아쉬웠다. 그 날씨에도 꿋꿋이
+      사진을 찍는 할머니가 계셨다. 비가 와서 카메라를 안 들고 다녔는데, 할머니를
+      보고 나도 카메라 방수 커버를 챙겨 사진을 찍어야겠다는 마음을 먹었다.
+      할머니 덕분에 리알토 비치에서 한 가지 얻은게 있었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/Fq4HrAI.png",
+        "https://i.imgur.com/i91rhAB.png",
+        "https://i.imgur.com/GQKIToz.png",
+      ]}
+      alt="리알토 비치"
+      caption="폐허 같았던 리알토 비치"
+    />
+    <YoutubeVideo
+      videoId="2ZOwigpB5Gc"
+      caption="바람이 많이 불어 파도가 시원하게 쳤다."
+    />
+    <YoutubeVideo videoId="ygsXrqY8ujQ" caption="시워언하다." />
+    <p>
+      호 열대우림으로 이동하기 전, 포크스에 들러 기념품 샵을 구경하고 가볍게
+      마그넷을 구입했다. 트와일라잇의 배경이 되었던 마을 중 하나라, 관련 굿즈가
+      많았다. 그리고 우린 기대하던 호 열대우림으로 이동했다. 호 열대우림도
+      날씨가 좋으면 더 좋았겠지만, 비가 와도 어울리는 숲이었기에 좀 나았다. 한
+      바퀴 도는 트레일이 있기에 걸었다. 리알토 비치에서 본 할머니를 본받아
+      카메라를 들고 다녔다. 쉽진 않았다. 방수 커버가 자꾸 렌즈를 덮어 사진에
+      자국이 생겼다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/r8J3GPg.jpeg"]}
+      alt="지도"
+      caption="호 열대우림에서 걸었던 트레일"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/NNzTiBf.jpeg"]}
+      alt="호 열대우림"
+      caption="처음 보는 세계"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ajwh2jz.jpeg"]}
+      alt="호 열대우림"
+      caption="비에 젖은 나무"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/xrNSUxH.jpeg"]}
+      alt="호 열대우림"
+      caption="또 다른 의미의 폐허 같았다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/sC25uHB.jpeg"]}
+      alt="나무"
+      caption="나무가 쓰러지면 치우지 않고, 사람이 다닐 수 있게끔만 만든다. 이거 좋다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/M5uAM0D.jpeg"]}
+      alt="나무"
+      caption="작은 사람이 지나다닐 수 있을 법한 나무 터널"
+    />
+    <p>
+      호 열대우림에서 나와 루비 비치를 들렀다. 리알토 비치와 차이를 알기 어려울
+      정도로 비슷했다. 날씨가 좋았다면, 차이가 느껴졌을텐데.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/aOWCLzl.png"]}
+      alt="바다"
+      caption="가까운 돌 섬인데 잘 안 보인다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ERVvF1R.png"]}
+      alt="바다"
+      caption="리알토 비치와 비슷한 루비 비치. 왜 루비인지."
+    />
+    <p>
+      올림픽 국립공원을 떠나기 전 퀴널트 열대우림으로 갔다. 가는 길이 어려웠다.
+      비가 많이 내려서인지 하천이 범람하기도 했다. 도보로 산책하긴 어렵다고
+      판단하여 사진만 찍고 빠르게 나왔다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/UWxq8GG.jpeg"]}
+      alt="퀴널트 열대우림"
+      height={450}
+      caption="하천 수위가 높아져서 다리를 건너기도 무서웠다."
+    />
+    <p>
+      숙소가 있는 애버딘으로 이동하여 체크인 후, 앞에 있는 식당에서 저녁 식사와
+      맥주를 걸쳤다. 치킨까스, 함박스테이크, 맥주 세 잔, 팁과 세금까지 합쳐
+      86.09불. 한국으로 치면 12~13만원 정도다. 여행이니까 이렇게 먹지.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/B9E4XkY.png"]}
+      alt="저녁식사"
+      height={450}
+      caption="콜라가 아니라 흑맥주다."
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/CHgQQzZG2HyQWF1f8"
+      title="사파리"
+      description="Olympic Game Farm Inc."
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/e2TZQbEhPXJYriDC9"
+      title="허리케인 릿지 올라가기 전 비지터 센터"
+      description="Olympic National Park Visitor Center"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/KfCc4ztexAPhJ6YGA"
+      title="허리케인 릿지"
+      description="Hurricane Ridge Visitor Center"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/4MvNPutGmfkDkwoQ8"
+      title="크레센트 호수"
+      description="Lake Crescent Pier"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/pn6VSSZbJtECJLYm6"
+      title="숲 속 우리만 있었던 숙소"
+      description="Misty Valley Inn"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/oiroVJJJSnkTgFCU9"
+      title="리알토 비치"
+      description="Rialto Beach"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/LZWhLJZ5XcNVGW8u9"
+      title="포크스 트와일라잇 기념품 샵"
+      description="Native To Twilight"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/7LdfJXKvyQs9GtEE9"
+      title="호 열대우림"
+      description="Hoh Rain Forest"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/Rfaj2SiKjHdgUnuTA"
+      title="루비 비치"
+      description="Ruby Beach"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/sp4mAgLF74PBV4c7A"
+      title="퀴널트 열대우림"
+      description="Quinault Rainforest"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/8asYDBrCuQM7CAdc6"
+      title="숙소 앞의 식당"
+      description="Billy's Bar & Grill"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/63/meta.tsx
@@ -0,0 +1,23 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "63",
+  title: "2-2. 날씨 운이 좋지 않았던 올림픽 국립공원",
+  slug: "olympic-national-park",
+  summary: "미국 서부 여행의 두번째, 올림픽 국립공원",
+  subCategoryId: "4",
+  author: "양가브리엘",
+  publishDate: "2026-08-07 01:21:00",
+  readTime: 5,
+  imageUrl: "https://i.imgur.com/ERVvF1R.png",
+  tags: [
+    "올림픽국립공원",
+    "올림픽게임팜",
+    "퀴널트열대우림",
+    "루비비치",
+    "호열대우림",
+    "허리케인릿지",
+    "크레센트호수",
+    "리알토비치",
+  ],
+};
