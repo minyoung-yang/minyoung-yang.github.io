fa1fc60dfb715aa26fa8308b9a3231b9f7afc9cd
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-08-12 00:27:40 +0900

    64번 포스팅

---
+++ src/data/posts/64/content.tsx
@@ -0,0 +1,452 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+import { YoutubeVideo } from "@/components/entry-detail/youtube-video";
+
+export const content = (
+  <>
+    <p>
+      2026년 2월 3일, 애버딘에서 포틀랜드를 향해 출발했다. 애버딘에서 머물렀던
+      숙소는 Chehalis 강과 Wishkah 강이 만나는 곳에 있었는데, 투숙객들이 방수
+      옷과 아이스 박스를 챙긴걸 봐서 낚시하기 좋은 곳 같았다. 12번 국도로 가다가
+      5번 국도로 갈아탄 후 남쪽으로 쭉 이동했다. 100마일 정도 이동했을 무렵,
+      스트레칭할 겸 롱뷰에 위치한 타겟을 구경했다. 타겟의 스타벅스에서 커피 한
+      잔 구입한 뒤, 다시 포틀랜드로 향했다. 코스트코에 들러 주유 후 포틀랜드의
+      더 그로토에 도착했다. 더 그로토는 천주교 시설이지만, 정원과 작품들이
+      있어서 관광 시설이기도 하다. "차에 귀중품을 두지 마시오." 주차장에 있던
+      경고문을 보고, 빠르게 구경했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/60Y3f9z.jpeg"]}
+      alt="더 그로토"
+      caption="매우 큰 암석에 굴을 만들었다. 천주교의 석굴암이다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/2L7LDt4.jpeg"]}
+      alt="더 그로토"
+      caption="어떤 성인이라고 한다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/JcSxHYG.jpeg"]}
+      alt="더 그로토"
+      caption="성경 이야기가 그림으로 여러 편 그려져 있다."
+    />
+    <p>
+      포틀랜드의 도심지를 구경하기 위해 홀푸즈 마켓으로 갔다. 미국의 유기농
+      상품만을 취급하는 마트이기에 구경하는 것도 재밌지만, 주차를 하기
+      위함이었다. 홀푸즈 마켓에서 20불 이상 결제 시, 주차 할인을 받았던 것으로
+      기억한다. 홀푸즈 마켓에서 가벼운 쇼핑 후, 옆의 서점으로 이동했다. 세계
+      최대 규모인 독립 서점이라고 한다. 읽진 않겠지만, 알록달록한 책들을
+      구경하는 재미로 시간을 보냈다. 서점 주변의 기념품 가게도 구경하며,
+      포틀랜드 마그넷을 구입했다. 언젠가 듣기로 포틀랜드는 세계에서 제일 살기
+      좋은 곳이라고 들었는데, 지금은 그렇지 않은 거 같았다. 이 번화한 곳을 걸을
+      때 와이프와 꼭 붙어 다니며, 절대로 떨어지지 말자고 할 정도로 노숙자가
+      많았다. 대마초 냄새도 많이 났다. 살기 좋은 곳은 아닌 거 같았다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/7w7dshq.png"]}
+      alt="서점"
+      caption="색감 좋은 책들이 가득했던 서점"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/vcUSafU.png"]}
+      alt="서점"
+      caption="한강의 채식주의자가 추천 서적으로 있었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/V1XfIif.png"]}
+      alt="포틀랜드"
+      caption="한글이 적혀있는 기념품 가게"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/vM0rI6d.jpeg"]}
+      alt="기념품 가게"
+      caption="이쁜 것이 많았지만 필요 없었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/oE0kEPs.png"]}
+      alt="더 그로토"
+      height={500}
+      caption="내가 좋아하는 마그넷. 이 중 몇 개는 우리 집 냉장고에 붙어있다."
+    />
+    <p>
+      호텔 체크인 후, 주변에 있는 아디다스 스토어에 방문했다. 이 스토어는 예약
+      후 입장이 가능한 곳이다. 가격표의 숫자가 꽤 커서 놀랄 수 있지만, 50%
+      할인이 된다. 그렇게 보면 비교적 저렴하다. 폭풍 쇼핑을 한 후, 숙소에
+      쇼핑백을 두고 다시 나와 저녁 식사를 위해 피자 가게로 갔다. 생맥주와 피자를
+      먹었다. 정말 정말 맛있었다. 배부르게 먹고 취기가 살짝 올랐는데도 47불 밖에
+      안 나왔다. 기분 좋게 하루를 마무리했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/ww03BZ6.jpeg"]}
+      alt="아디다스 스토어"
+      caption="아디다스 스토어에 있던 비르츠 유니폼"
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/5Enowr2.jpeg",
+        "https://i.imgur.com/E3NBtXv.jpeg",
+      ]}
+      alt="피자"
+      caption="인생 최고의 피자 중 하나였다."
+    />
+    <p>
+      다음 날은 먼저 호이트 아버리텀에 방문했다. 포틀랜드에 있는 큰 수목원이자
+      공원이다. 입장은 무료이고, 주차비가 있었다. 정확한 금액은 기억나지 않지만,
+      합리적인 가격이었다. 꾸미지 않았지만, 방치하지 않은 이 수목원 주변에 사는
+      사람들이 부러웠다. 아침 운동하는 러너도 봤고, 산책하는 사람들도 봤다. 한두
+      시간 정도 산책하고 자리를 떴다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/5rfBZDj.png"]}
+      alt="산"
+      caption="호이트 아버리텀에서 보이는 후드 산"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/dfgbRXt.jpeg"]}
+      alt="호이트 아버리텀"
+      caption="걷기 딱 좋았던 수목원"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/n5sqHCz.jpeg"]}
+      alt="벤치"
+      caption="북미권의 벤치에는 항상 누군가의 이름이 쓰여져 있다. 기부자이려나."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/bWruRKE.png"]}
+      alt="나무"
+      height={500}
+      caption="한 장면에 담고 싶은 나무였다. 하지만 실패."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/XMzSF6E.jpeg"]}
+      alt="새집"
+      caption="자연과 인조"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/1ExWbvN.jpeg"]}
+      alt="호이트 아버리텀"
+      caption="마지막은 방문자 센터에서 기념품을 산다."
+    />
+    <p>
+      다음 장소로 이동했다. 다음 장소는 우드번 아울렛. 여행가면 좋은 브랜드 옷
+      구입하는 것을 좋아한다. 평상시에는 유니클로나 탑텐 같은 가성비 좋은 SPA
+      브랜드를 구입하는데, 여행 중에는 소비에 둔감해지기 때문에 멋진 브랜드 옷을
+      장만하기에 좋다. 오리건 주의 특장점이 여기서 나오는데, 바로 판매세가 붙지
+      않는다는 것이다. 대부분의 나라에서 약 10%, 미국에서는 주마다 다르지만
+      8~10% 정도 세금이 붙는데, 오리건 주는 0이다. 세금이 없는 오리건 주에서,
+      옷이 더 저렴한 아울렛에서, 쇼핑하기 위해 우드번 아울렛에 방문했다. 기록을
+      보니, 오전 11시부터 오후 5시까지 우린 6시간 동안 폭풍 쇼핑을 했다.
+      노스페이스, 코치, 리바이스 등 다양한 브랜드를 비교적 저렴하게 구입할 수
+      있었다. 아울렛 주변의 숙소에 체크인하고, 주변에 있는 타코벨에서 저녁 식사
+      후 하루를 마무리했다.
+    </p>
+    <p>
+      다음 날 우린 다시 포틀랜드 방향으로 조금 돌아갔다. 이번 목적지는 더 코브
+      팰리세이즈 주립공원이었는데, 가는 길이 너무 좋았다. 마운트 후드를 기반으로
+      뷰와 길이 멋지게 이어졌다. 이것을 처음 느낀 것은, 가는 길에 작게 있는
+      뷰포인트였다. 약 3~4대만 주차가 가능한 공간이었는데, 가보니 아무도 없었다.
+      거기서 마운트 후드 방향을 바라보니, 정말 장관이었다. 우연치 않게 만난 가장
+      멋진 뷰포인트였다. 마운트 후드라는 산을 처음 들었지만 너무 감명 받았기에,
+      중간에 기념품 샵에 들러 마그넷을 하나 샀다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/25T8iHT.png"]}
+      alt="마운트 후드"
+      caption="내 인생 최고의 뷰포인트"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/Uj74kg5.png"]}
+      alt="마운트 후드"
+      caption="스트레칭만 하고 가려던 쉼터였는데, 인생 장소가 되어버렸다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/T9DpboA.jpeg"]}
+      alt="집"
+      caption="뷰포인트 바로 앞의 집. 여기 거주자는 이 뷰가 질릴까?"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/B9pm0HR.png"]}
+      alt="마운트 후드"
+      caption="더 가까운 뷰포인트였는데, 좀 아쉬웠다. 바람이 많이 불어서 색다르긴 했다."
+    />
+    <p>
+      더 코브 팰리세이즈 주립공원이 가까워지니 주변의 분위기가 달라졌다.
+      서부극의 사막 분위기가 생겼다. 황량한 땅에 강물이 흐르고 있었다. 어디선가
+      안좋은 냄새도 났다. 이 주변의 지형 자체에서 나는 것 같았다. 날씨가 좀
+      아쉬웠지만, 이런 뷰를 볼 수 있다는 것이 신박했다. 조금 더 이동하여
+      선사시대의 암각화도 볼 수 있었다. 운전하기에도 좋았고, 분위기도 좋았다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/nfA99Pp.png"]}
+      alt="다리"
+      caption="이 다리를 기점으로 서부극 분위기가 시작되었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/zIyWqS1.png"]}
+      alt="사막"
+      caption="디아블로가 생각나는 분위기"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/46ofKTe.png"]}
+      alt="강"
+      caption="사막은 아니다. 강이 흐른다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/2bg6mJy.jpeg"]}
+      alt="가마우지"
+      caption="이 나라에는 가마우지가 곳곳에 있다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/neoVQb6.png"]}
+      alt="강"
+      caption="나무가 있냐 없냐에 따라 분위기가 다르다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/tNo77wB.jpeg"]}
+      alt="암각화"
+      caption="내가 손가락으로 그린게 아니라, 선사시대에 그려진 암각화이다."
+    />
+    <p>
+      주립공원 안에 있는 컬버에서 식사 후, 숙소로 가기 위해 남쪽으로 이동했다.
+      필롯 뷰트라는 곳에 잠시 방문하려 했지만, 입구에서 밴 당했다. 겨울에는
+      오픈하지 않는다고 한다. 조금 더 이동해서 뉴베리 국립 화산 기념물로 갔다.
+      가벼운 트레일을 돌았는데 상당히 시원하고 기분 좋았다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/rvLW1WI.png"]}
+      alt="길"
+      caption="저 멀리 마운트 후드가 보인다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ggxkI3I.png"]}
+      alt="뷰포인트"
+      caption="트레일 끝에서는 이런 뷰가 보인다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/SKkmyVY.png"]}
+      alt="소나무"
+      caption="외로운 소나무가 한 그루 있다."
+    />
+    <p>
+      숙소 체크인 후, 주변에 있는 사이더 양조장에 갔다. 사이더는 사과를 발효해서
+      만든 탄산 술이다. 입에 맞지는 않았지만, 우리가 언제 사이더 양조장에 갈 수
+      있겠냐는 생각으로 맛있게 먹었다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/Imtfw8I.png",
+        "https://i.imgur.com/X2eLu1e.png",
+        "https://i.imgur.com/AWI5rYi.png",
+      ]}
+      alt="사이더 양조장"
+      caption="내가 여기 로컬을 제대로 즐기고 있다는 느낌이 들었다."
+    />
+    <p>
+      다음 날, 크레이터 레이크 국립공원으로 향했다. 120마일을 이동해야 했기에,
+      중간에 쉬는 포인트를 조금씩 뒀다. 그 중 기억에 남는 포인트는 길 한복판에
+      있었는데, 잠시 주차 후 차가 별로 없는 차도에서 저 멀리까지 볼 수 있는
+      뷰포인트였다. 이번 여행에서 찍은 사진 중 제일 좋아하는 사진들이 여기서
+      나왔다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/bMzz3DQ.jpeg"]}
+      alt="도로"
+      caption="누군가가 귀여운 고양이를 그려놨다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/4BwQeDB.png"]}
+      alt="초원"
+      caption="뛰어도 뛰어도 도착 못 할 것만 같은 곳"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/U2otrXe.png"]}
+      alt="도로"
+      caption="이 도로에 끝이 있을까? 어떻게 생겼을까?"
+    />
+    <p>
+      뷰포인트에서 얼마 가지 않아, 크레이터 레이크 국립공원에 도착했다. 이
+      국립공원에 오기 전까지 많은 걱정을 했다. 매일 국립공원 관리공단 홈페이지에
+      접속하여, 실질적으로 크레이터 레이크를 볼 수 있는 장소까지 이동하는 림
+      드라이브가 열렸는지 확인했다. 미국으로 떠나기 전까지는 폭설로 인해
+      닫혀있었다. 다행히 미국에 와서 보았을 땐 열려있었다. 직접 가보니, 도로만
+      눈이 치워져 있었고 그 외에는 내 키보다 높에 눈이 쌓여있었다. 림 드라이브를
+      통해 호수를 한 바퀴 돌면 좋았겠지만, 겨울에는 열리지 않는 길이었기에 림
+      비지터센터까지만 갔다. 한 눈에 담기 힘든 호수가 있었고, 그 호수는 한 눈에
+      담기 힘든 산으로 둘러싸여 있었다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/MtVflif.png"]}
+      alt="호수"
+      caption="한 컷에 찍기 어려워서 반씩 잘라 찍었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/LyQpUhE.png"]}
+      alt="호수"
+      caption="정말 환상적인 호수"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/yuQwEtL.jpeg"]}
+      alt="호수"
+      caption="똥폼 잡는 와이프"
+    />
+    <p>
+      아쉽지만 예약해둔 숙소로 해가 지기 전에 이동하려면 떠나야 한다. 호수
+      앞에서 감탄하다가 숙소 방향으로 이동했다. 가는 길에 Rogue Gorge라는 공원이
+      있었다. 잠시 내려 작은 강을 따라 조금 걸었다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/dUAin3t.jpeg"]}
+      alt="표지판"
+      caption="Rogue는 강 이름이고 Gorge는 폭포 이름이라는 추측 중이다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/RcJGAjh.jpeg"]}
+      alt="강"
+      caption="시원하게 흐르는 강"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/TMXpkNg.jpeg"]}
+      alt="강"
+      caption="물은 빠르고 나무는 많다."
+    />
+    <YoutubeVideo videoId="nmBzUhyOpLo" caption="기분이 좋아지는 시원함" />
+    <p>
+      조금 더 이동하여 Lost Creek 호수에서도 잠시 시간을 가졌다. 휴대용 캠핑
+      의자를 깔고 호수를 바라보는 두 노인을 보며 우리도 저렇게 늙고 싶다는
+      이야기도 했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/JiRwDfP.png"]}
+      alt="호수"
+      caption="계획하지 않았던 장소이기에 더 반갑고 즐거웠던 장소"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/CBtVGce.png"]}
+      alt="호수"
+      caption="고요했다."
+    />
+    <p>
+      숙소가 있는 그랜츠패스까지 이동했다. 전 날 양조장에서 맛있는 경험을
+      했기에, 이번에도 주변의 양조장으로 갔다. 맛있는 맥주로 하루를 마무리했다.
+      이제 주 경계를 넘어 캘리포니아로 넘어갈 것이다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/gCYcgGd.png",
+        "https://i.imgur.com/o1e8qhy.png",
+      ]}
+      alt="양조장"
+      caption="직접 맥주를 만들어 파는 곳"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/fsEzveepB682qgFF8"
+      title="천주교의 석굴암"
+      description="The Grotto"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/MP1m4sTZAMKzYK4P9"
+      title="세계 최대 독립 서점"
+      description="Powell's City of Books"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/meNRoYNDEYGTmzWw8"
+      title="한글이 적힌 기념품 샵"
+      description="Hello From Portland"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/2vC26DpVw8y7Umtr6"
+      title="색감 좋은 기념품 샵"
+      description="MadeHere"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/VnNGQ12M1xLoKqjcA"
+      title="예약 필수인 아디다스 스토어"
+      description="Adidas Employee Store Portland"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/9VpzWm9kyzd7ZGW4A"
+      title="인생 피자집"
+      description="Life of Pie"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/fAbLKNEg6uArjnCx9"
+      title="수목원"
+      description="Hoyt Arboretum"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/9AB1b7xZuhKLqDCN6"
+      title="판매세 안 붙는 아울렛"
+      description="Woodburn Premium Outlets"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/1U1QpaSVUsaMdJeN6"
+      title="인생 뷰포인트"
+      description="Jonsrud Viewpoint"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/rLgGkMQBcXG4xAHx5"
+      title="서부극의 기점이 되는 다리"
+      description="Mill Creek Bridge"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/8JWvPJVnetvF29V97"
+      title="서부극의 배경이 될 법한 주립공원"
+      description="The Cove Palisades State Park"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/V46skUh9nxSXJiVWA"
+      title="뉴베리 국립 화산 기념물"
+      description="Newberry National Volcanic Monument"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/BoQQYjWm7Gmhyxnc7"
+      title="사이더 양조장"
+      description="Legend Cider Company"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/fEj9YvWRd3n2g1J6A"
+      title="인생컷을 찍은 뷰포인트"
+      description="View Point"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/iKikBjysFqS7GW3SA"
+      title="크레이터 레이크 국립공원"
+      description="Crater Lake National Park"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/7XWdK91c7H3PGTpQA"
+      title="Rogue Gorge"
+      description="Rogue River Gorge Viewpoint"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/MMcexYhhF4thnodQ6"
+      title="Lost Creek Lake"
+      description="Lost Creek Lake Viewpoint"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/XdURMFCvJ5Ggfi8n9"
+      title="맥주 양조장"
+      description="WEEKEND BEER COMPANY"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/64/meta.tsx
@@ -0,0 +1,20 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "64",
+  title: "2-3. 오리건 주 여행을 따로 오고 싶다.",
+  slug: "oregon-state-visit",
+  summary: "미국 서부 여행의 세번째, 포틀랜드를 거쳐 팰리세이즈 주립공원",
+  subCategoryId: "4",
+  author: "양가브리엘",
+  publishDate: "2026-08-12 00:26:00",
+  readTime: 8,
+  imageUrl: "https://i.imgur.com/U2otrXe.png",
+  tags: [
+    "미국",
+    "오리건",
+    "포틀랜드",
+    "크레이터레이크국립공원",
+    "더코브팰리세이즈주립공원",
+  ],
+};
