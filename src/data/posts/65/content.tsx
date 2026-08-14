5aabde8923158a1212b94548b84d93bb3dc9b252
gabriel.yang <gabriel.yang@kakaocorp.com>
2026-08-14 12:57:34 +0900

    65번 포스팅

---
+++ src/data/posts/65/content.tsx
@@ -0,0 +1,256 @@
+"use client";
+
+import { ImageInEntry } from "@/components/entry-detail/image-in-entry";
+import { OgLink } from "@/components/entry-detail/og-anchor";
+import { YoutubeVideo } from "@/components/entry-detail/youtube-video";
+
+export const content = (
+  <>
+    <p>
+      2026년 2월 7일, 그랜츠패스에서 레드우즈 주립공원을 향해 출발했다. 중간에
+      Dollar General에 들러 군것질 거리와 필요한 동전 배터리를 구입했다. 미국의
+      다이소 같은 곳으로 생각하고 들렀는데, 가격이 저렴하진 않았다. 내가 찾는
+      건전지가 매대에 품절이었는데, 직원분께서 다른 매대에서 찾아다주었다.
+      친절함에 감사했다. 캘리포니아로 넘어가는 주 경계에는 검문소가 있었다.
+      창문을 열고 질문에 답했다. "과일 있냐?", "바나나 있다.", "OK 가라". 별
+      문제 없이 10초만에 검문을 통과했다. 히우치 방문자 센터에 도착하여 화장실을
+      이용하고, 기념품 샵을 구경했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/tplH3k4.png"]}
+      alt="인형"
+      height={400}
+      caption="레드우드 국립공원의 상징인 바나나 민달팽이 인형"
+    />
+    <p>
+      레드우드 숲을 탐험할 때다. 여기는 제다이아 스미스 레드우즈 주립공원과
+      레드우드 국립공원이 붙어있다. 방문자센터에 방문해도 두 공원이 함께 각인된
+      기념품도 있다. 레드우드 국립공원은 남북으로 길게 생겼는데, 북쪽의 동쪽에
+      붙어있는 숲이 주립공원인 것으로 이해하고 있다. 우린 북쪽으로부터
+      내려갔기에, 주립공원을 먼저 들르게 되었다. Howland Hill Road를 통해
+      주립공원을 통과하며 여러 트레일을 트레킹했다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/Knm3vQI.jpeg"]}
+      alt="Stout Grove"
+      caption="Stout Grove Trail은 시작부터 압도된다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/L18ipuU.jpeg"]}
+      alt="Stout Grove"
+      caption="나무가 쓰러졌다고 치우지 않는다. 사람이 갈 길만을 뚫는다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/mHcZrzY.jpeg"]}
+      alt="Stout Grove"
+      caption="뿌리부터 사이즈가 나온다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/OfcCvHx.jpeg"]}
+      alt="Stout Grove"
+      caption="나무를 따라 걸어간다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/wdHgYCU.jpeg"]}
+      alt="Grove of Titans"
+      caption="Grove of Titans를 알리는 표지판"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/sOdkdKo.jpeg"]}
+      alt="Grove of Titans"
+      caption="초장부터 베어져있는 나무"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/z7S00ix.png"]}
+      alt="Grove of Titans"
+      height={800}
+      caption="말 그대로 타이탄 같은 나무들이 가득하다."
+    />
+    <p>
+      크레센트 시티에 도착하여 체크인 후 동네를 구경했다. 등대가 있고,
+      24시간동안 몇 초에 한 번 주기적으로 등대 소리가 나는 도시다. 초반엔 거슬릴
+      수 있지만 몇 번 듣다 보면 괜찮다. 작지만 볼 것이 많은 동네였다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/5C8XS88.jpeg"]}
+      alt="벽화"
+      caption="크레센트 시티의 상징인 등대가 그려져있다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/KyOdqMt.png"]}
+      alt="등대"
+      caption="크레센트 시티의 상징인 등대다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/oFblZBU.png"]}
+      alt="벚꽃나무"
+      height={500}
+      caption="2월에 벚꽃을 봤다."
+    />
+    <YoutubeVideo
+      videoId="8cgDQKSXiqU"
+      caption="아마도 마멋일거다. 마멋이 많은 동네였다."
+    />
+    <p>
+      다음날 우리는 레드우드 국립공원을 통해 남쪽으로 이동했다. 아쉽게도 비가 꽤
+      왔다. 내가 국립공원을 가는 날이면 비가 오나보다. 레드우드답게 큰 나무로
+      가득 차 있었다.
+    </p>
+    <YoutubeVideo
+      videoId="saMrrU3iOlY"
+      caption="Klamath River Overlook. 강과 바다가 만나는 곳이다."
+    />
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/hlANxAv.png",
+        "https://i.imgur.com/Y8QYEps.png",
+        "https://i.imgur.com/KtUAhAe.png",
+      ]}
+      alt="나무"
+      caption="큰 나무, 더 큰 나무, 제일 큰 나무..."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/SleVP6I.jpeg"]}
+      alt="엘크"
+      caption="Prairie Creek Visitor Center에서는 엘크를 많이 볼 수 있다."
+    />
+    <YoutubeVideo
+      videoId="-wkByGq-IIE"
+      caption="방문자센터에서 가까운 곳에 있던 Trillium Falls. 비가 와서 흙탕물이 됐다."
+    />
+    <p>
+      기대하던 트레일이었던 Tall Trees Grove로 이동했다. 이 트레일은 NPS를 통해
+      미리 허가를 받아야지만 접근할 수 있는 트레일이다. 그렇다는 점이 나를 더
+      기대하게 만들었다. 우리가 방문하는 2026년 2월 8일에 맞춰 예약했고, 그 날
+      새벽 2시에 입장 코드가 이메일로 왔다. 매일 자물쇠 비밀번호가 변경되고
+      예약자들에게 오전 일찍 알려주는 것이다. 하지만 비가 많이 와서일까. 다리는
+      피곤하고 몸은 젖었지만 눈은 즐겁지 않은 그런 트레일이었다. 평가가 좋은
+      트레일은, 피로에 비해 눈이 즐겁기 때문인데 비가 와서 즐겁지 않았다. 방수
+      외투도 입고 갔지만, 외투를 뚫고 옷이 젖었다. 저 트레일에 가본 사람이
+      되었다는 점 외에는 뭐가 없었다. 약 두 시간의 트레킹 후 숙소가 있는
+      맥킨리빌로 이동하였다.
+    </p>
+    <ImageInEntry
+      src={[
+        "https://i.imgur.com/qOduYRs.png",
+        "https://i.imgur.com/hxERymi.png",
+      ]}
+      alt="Tall Trees Grove"
+      caption="큰 나무와 도마뱀을 봤다."
+    />
+    <p>
+      다음 날, 우리는 맥킨리빌을 떠나 샌프란시스코로 향했다. 약 300마일을
+      가야하는 장거리코스였기에 중간마다 쉬는 포인트를 잘 찾아보고 이동했다.
+      하지만 쉰다고 되는 건 아니었다. 뷰포인트에서 스트레칭은 가능했지만,
+      공중화장실이 없었다. 우리는 이동 중에 갑작스런 복부의 신호를 받았고,
+      화장실을 찾아봤지만 찾을 수 없었다. 구글 맵을 통해 찾아보았다. 입장료가
+      있는 별 볼 일 없는 관광지였기에 피하려던 곳을 찾았고, 빠르게 이동하여
+      화장실을 이용했다. 15불짜리 화장실이었다. 오전 8시 30분에 출발하여
+      샌프란시스코 트윈피크스에 16시에 도착했다. 7시간이 넘는 이동이었다.
+    </p>
+    <ImageInEntry
+      src={["https://i.imgur.com/cCOzmWt.jpeg"]}
+      alt="자동차"
+      caption="잠시 멈춰 몸을 펴준다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/jl48NR4.png"]}
+      alt="큰 나무"
+      caption="1800년 된 Grandfather Tree. 바로 앞에는 기념품 샵이 있다."
+      height={600}
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/1KeugoA.png"]}
+      alt="공원"
+      caption="급히 화장실을 이용하기 위해 15불 내고 입장한 곳"
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/ifNHDlX.jpeg"]}
+      alt="새"
+      caption="원래는 차로 나무 사이를 지나가는 코스이지만, 사진을 못 찍었다."
+    />
+    <ImageInEntry
+      src={["https://i.imgur.com/vrHJ0hd.jpeg"]}
+      alt="스누피"
+      caption="샌프란시스코 도착 직전에 방문한 샌타로자의 스누피 아이스링크"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/wsaZh4wLBgv6iaYHA"
+      title="미국의 다이소이지만 비싼 곳"
+      description="Dollar General"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/kKuJE257M2ig9AWz5"
+      title="바나나 조사 받았던 검문소"
+      description="California Agricultural Inspection Station - Redwood"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/DsCXij5KzFnk1EvQ7"
+      title="히우치 방문자 센터"
+      description="Hiouchi Visitor Center"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/fa86CDDdBVTquKSw7"
+      title="제다이아 스미스 레드우즈 주립공원의 첫 트레일"
+      description="Stout Grove"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/FR2ffXYi2dBRzke57"
+      title="제다이아 스미스 레드우즈 주립공원의 두번째 트레일"
+      description="Grove of Titans Trailhead"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/eoFqjGSvStwpHm4E9"
+      title="크레센트 시티의 상징인 등대"
+      description="Battery Point Lighthouse and Museum/Crescent City Lighthouse"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/uTEzkh1cie1uKKqi8"
+      title="Klamath River Overlook"
+      description="Klamath River Overlook"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/TkJDEkq3vKovpGi48"
+      title="엘크가 많이 나오는 방문자 센터"
+      description="Prairie Creek Visitor Center"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/UvESPKxoYMWzApMFA"
+      title="작은 폭포가 있는 트레일"
+      description="Trillium Falls Trail"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/MLa7Ebr49TREnwTe7"
+      title="예약해야 들어갈 수 있는 트레일"
+      description="Tall Trees Grove"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/FsEGEw6UHodvDipj8"
+      title="할아버지 나무"
+      description="The Grandfather Tree"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/5Web3vdFnm4Qg3R46"
+      title="15불 내고 화장실을 이용한 관광지"
+      description="Drive-Thru Tree Park Leggett"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+    <OgLink
+      href="https://maps.app.goo.gl/2ECYTv9WztN4Hp9a6"
+      title="스누피 아이스링크"
+      description="Snoopy's Home Ice"
+      image="https://i.ytimg.com/vi/ZG4JQX4BO9A/maxresdefault.jpg"
+    />
+  </>
+);
---
+++ src/data/posts/65/meta.tsx
@@ -0,0 +1,20 @@
+import { PostMeta } from "@/types/blog";
+
+export const postMeta: PostMeta = {
+  id: "65",
+  title: "2-4. 레드우드 국립공원을 거쳐 샌프란시스코로 가는 길",
+  slug: "redwood-national-park-to-san-francisco",
+  summary:
+    "미국 서부 여행의 네번째, 레드우드 국립공원을 거쳐 샌프란시스코로 이동하다.",
+  subCategoryId: "4",
+  author: "양가브리엘",
+  publishDate: "2026-08-14 12:55:00",
+  readTime: 6,
+  imageUrl: "https://i.imgur.com/L18ipuU.jpeg",
+  tags: [
+    "미국",
+    "캘리포니아",
+    "제다이아스미스레드우즈주립공원",
+    "레드우드 국립공원",
+  ],
+};
